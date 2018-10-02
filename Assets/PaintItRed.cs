6c0c2d92ebd3b12f6e69495240372d8a7ab2e5a5
gaga <23i03g@mail.ru>
2018-10-02 17:12:27 +0300

    Initial commit

---
+++ Assets/PaintItRed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintItRed : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            print("ROSES ARE RED!");
+        }
+    }
+}
