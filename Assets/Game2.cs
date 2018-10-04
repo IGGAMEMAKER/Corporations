618c0415c0670288426eecf5c4a6a242fd6aa2a6
gaga <23i03g@mail.ru>
2018-10-05 00:06:22 +0300

    study hard, work easy! (Lists)
    trying to rememer List syntax

---
+++ Assets/Game2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Game2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int featureCount = 4;
+
+        List<int> features = Enumerable.Repeat(0, featureCount).ToList();
+
+        Debug.Log("Printing elements!");
+        for (int i = 0; i < features.Count; i++)
+        {
+            Debug.Log("Printing element[" + i + "]: " + features[i]);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
