7c5a79ebd8d9285dcdabf22964950538df5014a0
gaga <23i03g@mail.ru>
2018-10-17 22:57:42 +0300

    first instantiating attempt (Danger Interrupts)

--- Assets/Game2.cs
+++ Assets/Game2.cs
@@ -6,10 +6,22 @@ using UnityEngine;
 
 public class Game2 : MonoBehaviour {
     World world;
+    public const float interval = 5f;
+
+    float totalTime = interval; //2 minutes
+
+    GameObject DangerInterrupt;
+    private float spacing = 50f;
+    GameObject Canvas;
+    int Cell;
 
     // Use this for initialization
     void Start () {
         world = new World();
+        DangerInterrupt = GameObject.Find("DangerInterrupt");
+        Canvas = GameObject.Find("Canvas");
+
+        Cell = 0;
 
         int projectId = 0;
         int featureId = 0;
@@ -35,10 +47,34 @@ public class Game2 : MonoBehaviour {
         world.RaiseInvestments(projectId, 20, 150000);
         world.PrintShareholders(projectId);
         world.PeriodTick(32);
+
+
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        totalTime -= Time.deltaTime;
+
+        if (totalTime < 0)
+        {
+            UpdateLevelTimer(totalTime);
+
+            totalTime = interval;
+            Cell++;
+        }
+    }
+
+    public void UpdateLevelTimer(float totalSeconds)
+    {
+        for (int y = 0; y < 1; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Vector3 pos = new Vector3(x, 0, (y - Cell)) * spacing;
+                GameObject g = Instantiate(DangerInterrupt, pos, Quaternion.identity);
+
+                g.transform.SetParent(Canvas.transform);
+            }
+        }
+    }
 }
