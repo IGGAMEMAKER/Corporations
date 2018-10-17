18c92b01350df3ccdaf239a3019b7252f28c4673
gaga <23i03g@mail.ru>
2018-10-17 23:49:15 +0300

    moved unity scene experiments to separate file (TestingScript)

--- Assets/Game2.cs
+++ Assets/Game2.cs
@@ -6,26 +6,10 @@ using UnityEngine;
 
 public class Game2 : MonoBehaviour {
     World world;
-    public const float interval = 5f;
-
-    float totalTime = interval; //2 minutes
-
-    GameObject DangerInterrupt;
-    private float spacing = 50f;
-    GameObject Canvas;
-    int Cell;
-
-    List<GameObject> Interrupts;
 
     // Use this for initialization
     void Start () {
         world = new World();
-        DangerInterrupt = GameObject.Find("DangerInterrupt");
-        Canvas = GameObject.Find("Canvas");
-
-        Interrupts = new List<GameObject>();
-
-        Cell = 0;
 
         int projectId = 0;
         int featureId = 0;
@@ -57,37 +41,7 @@ public class Game2 : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        totalTime -= Time.deltaTime;
 
-        if (totalTime < 0)
-        {
-            UpdateLevelTimer(totalTime);
-
-            totalTime = interval;
-            Cell++;
-        }
     }
 
-    public void UpdateLevelTimer(float totalSeconds)
-    {
-        for (int y = 0; y < 1; y++)
-        {
-            for (int x = 0; x < 5; x++)
-            {
-                Vector3 pos = new Vector3(x, (y - Cell), 0) * spacing;
-                GameObject g = Instantiate(DangerInterrupt, pos, Quaternion.identity);
-
-                g.transform.SetParent(Canvas.transform, false);
-
-                Interrupts.Add(g);
-            }
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            var g = Interrupts[i];
-            Destroy(g);
-        }
-        Interrupts.RemoveRange(0, 4);
-    }
 }
