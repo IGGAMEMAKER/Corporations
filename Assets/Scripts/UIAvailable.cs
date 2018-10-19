b4ce9091bb7ab710bd45740be481c548f3831965
gaga <23i03g@mail.ru>
2018-10-19 21:47:22 +0300

    still struggling with overlap

--- Assets/Scripts/UIAvailable.cs
+++ Assets/Scripts/UIAvailable.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIAvailable : MonoBehaviour {
-    Image background;
-
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start () {
         GameObject image = new GameObject("background");
-        image.AddComponent<Image>();
+        Image background = image.AddComponent<Image>();
+        Canvas c = image.AddComponent<Canvas>();
+
+        c.overrideSorting = true;
+        c.sortingOrder = -1;
 
         var parent = this.gameObject;
         image.transform.SetParent(parent.transform, false);
-        background = image.GetComponent<Image>();
-        background.sprite = Resources.Load<Sprite>("interrupt-danger");
-
-        // move it to the background layer
-        image.transform.Translate(new Vector3(0, 0, 1000));
 
         RectTransform parentTransform = parent.GetComponent<RectTransform>();
         float width = parentTransform.rect.width + 100f;
@@ -25,6 +23,8 @@ public class UIAvailable : MonoBehaviour {
 
         RectTransform rect = image.GetComponent<RectTransform>();
         rect.rect.Set(0, 0, width, height);
+
+        background.sprite = Resources.Load<Sprite>("interrupt-danger");
     }
 	
 	// Update is called once per frame
