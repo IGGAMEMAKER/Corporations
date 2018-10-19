bef0c05cae5fd75eb570c06ba466a26e9465bc13
gaga <23i03g@mail.ru>
2018-10-19 21:10:33 +0300

    Achieving some progress with UIAvailable. But Background image still overlaps Foreground Image

---
+++ Assets/Scripts/UIAvailable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAvailable : MonoBehaviour {
+    Image background;
+
+	// Use this for initialization
+	void Start () {
+        GameObject image = new GameObject("background");
+        image.AddComponent<Image>();
+
+        var parent = this.gameObject;
+        image.transform.SetParent(parent.transform, false);
+        background = image.GetComponent<Image>();
+        background.sprite = Resources.Load<Sprite>("interrupt-danger");
+
+        // move it to the background layer
+        image.transform.Translate(new Vector3(0, 0, 1000));
+
+        RectTransform parentTransform = parent.GetComponent<RectTransform>();
+        float width = parentTransform.rect.width + 100f;
+        float height = parentTransform.rect.height + 100f;
+
+        RectTransform rect = image.GetComponent<RectTransform>();
+        rect.rect.Set(0, 0, width, height);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
