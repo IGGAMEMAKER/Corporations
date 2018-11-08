395a975248b3cb511bec9a798ff2ad01e3ce927b
gaga <23i03g@mail.ru>
2018-11-09 01:39:31 +0300

    refactoring AudioManager

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -8,31 +8,18 @@ namespace Assets
 {
     public class AudioManager: MonoBehaviour
     {
-        AudioSource audioData;
-        List<AudioSource> channels;
         Dictionary<AudioClip, AudioSource> sources;
 
         public AudioClip coinSound;
-        int SOUND_COIN = 0;
-
         public AudioClip standardClickSound;
-        int SOUND_STANDARD_CLICK = 1;
-
         public AudioClip notificationSound;
-        int SOUND_NOTIFICATION = 2;
-
         public AudioClip toggleScreenSound;
-        int SOUND_TOGGLE_SCREEN = 3;
-
         public AudioClip toggleButtonSound;
-        int SOUND_TOGGLE_BUTTON = 4;
 
         void Start()
         {
-            channels = new List<AudioSource>();
             sources = new Dictionary<AudioClip, AudioSource>();
             
-            audioData = gameObject.AddComponent<AudioSource>();
             AddSound(coinSound);
             AddSound(standardClickSound);
             AddSound(notificationSound);
@@ -45,20 +32,11 @@ namespace Assets
             AudioSource audioSource = gameObject.AddComponent<AudioSource>();
             audioSource.clip = audioClip;
 
-            channels.Add(audioSource);
             sources[audioClip] = audioSource;
-        }
-
-        void PlaySound(int sound)
-        {
-            channels[sound].Play();
         }
 
         void Play(AudioClip clip)
         {
-            //audioData.clip = clip;
-            //audioData.Play();
-
             sources[clip].Play();
         }
 
