5f0f8e7aa8d0b31f9b1740255e692ab5ae1cbeaa
gaga <23i03g@mail.ru>
2018-12-09 19:38:31 +0400

    Reworking WorkerView

--- Assets/WorkerView.cs
+++ Assets/WorkerView.cs
@@ -6,43 +6,41 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class WorkerView : MonoBehaviour {
-    public ProgressBar MoraleProgressBar;
-    public UIHint MoraleProgressHint;
+    public ProgressBar DemoralisationProgressBar;
+    public UIHint DemoralisationHint;
+
+    public Text MoraleText;
+    public UIHint MoraleHint;
+    public UIHint SkillsetHint;
+
+    public UIHint SkillProgressionHint;
 
     void RenderMorale (int morale)
     {
-        GameObject MoraleValue = gameObject.transform.Find("MoraleValue").gameObject;
-        Text component = MoraleValue.GetComponent<Text>();
-
         string text;
 
         if (morale > 30)
         {
             text = "High";
-            component.color = Color.green;
+            MoraleText.color = Color.green;
         } else if (morale > 0)
         {
             text = "Normal";
-            component.color = Color.gray;
+            MoraleText.color = Color.gray;
         } else
         {
             text = "Terrible";
-            component.color = Color.red;
+            MoraleText.color = Color.red;
         }
 
-        component.text = text + " (" + morale + ")";
+        MoraleText.text = text + " (" + morale + ")";
     }
 
     void RenderMoraleProgressBar(Human human, int workerMorale, int teamMorale)
     {
-        //GameObject MoraleBar = gameObject.transform.Find("ProgressBar").gameObject;
-
         // hide progressBar if morale is negative
-        MoraleProgressBar.enabled = workerMorale < 0;
-        //MoraleBar.SetActive(workerMorale < 0);
-
-        //ProgressBar progressBar = MoraleBar.GetComponent<ProgressBar>();
-        MoraleProgressBar.SetValue(human.DesireToLeave);
+        DemoralisationProgressBar.gameObject.SetActive(workerMorale < 0);
+        DemoralisationProgressBar.SetValue(human.DesireToLeave);
 
         int moraleChange = human.GetMoraleChange(teamMorale);
 
@@ -52,8 +50,7 @@ public class WorkerView : MonoBehaviour {
             "\nThis worker will stop working" +
             "\nin {1} months", moraleChange, monthsToDemoralize);
 
-        //MoraleProgressBar.gameObject.GetComponentInChildren<UIHint>().SetHintObject(hint);
-        MoraleProgressHint.SetHintObject(hint);
+        DemoralisationHint.SetHintObject(hint);
     }
 
     string GetSignedValue (int value)
@@ -66,9 +63,6 @@ public class WorkerView : MonoBehaviour {
 
     void RedrawMoraleHint(Human human, int teamMorale)
     {
-        GameObject MoraleValue = transform.Find("MoraleValue").gameObject;
-        UIHint MoraleHint = MoraleValue.GetComponentInChildren<UIHint>();
-
         string hintText = String.Format(
             "Base Morale: {0} \n\n"+
             "Team Morale: {1} \n"+
@@ -83,9 +77,6 @@ public class WorkerView : MonoBehaviour {
 
     void RenderSkills(Human human)
     {
-        GameObject Avatar = transform.Find("Name").gameObject;
-        UIHint SkillsetHint = Avatar.GetComponentInChildren<UIHint>();
-
         string hintText = String.Format(
             "          {3}         \n\n" +
             "<b>Management</b>  - {0} LVL \n" +
@@ -152,9 +143,9 @@ public class WorkerView : MonoBehaviour {
             human.SpecialisationSkill.RequiredXP
             );
 
-        UIHint uIHint = progressBar.GetComponentInChildren<UIHint>();
-        uIHint.SetHintObject(hint);
-        uIHint.Rotate(-90);
+        //UIHint uIHint = progressBar.GetComponentInChildren<UIHint>();
+        SkillProgressionHint.SetHintObject(hint);
+        SkillProgressionHint.Rotate(-90);
     }
 
     public void UpdateView(Human human, int index, Dictionary<string, object> parameters)
