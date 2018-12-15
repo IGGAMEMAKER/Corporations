a394c5cddcb6d0649985e373dc637d955f79435d
gaga <23i03g@mail.ru>
2018-12-15 11:18:27 +0400

    Complexity

---
+++ Assets/ComplexityView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComplexityView : MonoBehaviour {
+    GameObject ComplexityPrefab;
+
+    void Start()
+    {
+        ComplexityPrefab = Resources.Load<GameObject>("Assets/Prefabs/ComplexityCost");
+    }
+
+    public void Render(int complexity)
+    {
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+
+        for (var i = 0; i < complexity; i++)
+            Instantiate(ComplexityPrefab, transform);
+    }
+}
