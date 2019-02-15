0f1eb6f45e5f24531f262ebe5c708819010e9f65
gaga <23i03g@mail.ru>
2019-02-15 21:49:15 +0400

    updates in niche, screen title and business view

---
+++ Assets/Niche.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Niche : MonoBehaviour
+{
+    RectTransform RectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform = GetComponent<RectTransform>();
+
+        float size = Random.Range(0.58f, 1.35f);
+
+        RectTransform.localScale = new Vector3(size, size, size);
+    }
+}
