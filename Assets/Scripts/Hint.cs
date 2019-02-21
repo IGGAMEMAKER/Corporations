67f6042d32f311cf72c55c28f14a459d22cf7573
gaga <23i03g@mail.ru>
2019-02-21 18:45:27 +0400

    refactoring Hint

--- Assets/Scripts/Hint.cs
+++ Assets/Scripts/Hint.cs
@@ -27,59 +27,64 @@ public class Hint : MonoBehaviour
     //    text.Split('\n',);
     //}
 
+    void ShowHint()
+    {
+        if (Text.Length == 0)
+            return;
+
+        InitStyles();
+
+        //GUIStyle guiStyle = new GUIStyle();
+        //Font font = new Font();
+        //font
+        //guiStyle.font = font;
+        //guiStyle.fontSize = 20;
+
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Screen.height - Input.mousePosition.y;
+
+        float offsetX = 15f;
+        float offsetY = 15f;
+
+
+        float contentWidth = 225f;
+        float contentHeight = 225f;
+
+        float rightSideOfScreenOffset = 0;
+
+        if (mouseX > Screen.width - contentWidth)
+            rightSideOfScreenOffset = contentWidth;
+
+        Rect content = new Rect(mouseX + offsetX - rightSideOfScreenOffset, mouseY, contentWidth, contentHeight);
+        Rect wrapper = new Rect(mouseX - offsetX - rightSideOfScreenOffset, mouseY - offsetY, contentWidth + offsetX, contentHeight + offsetY);
+
+        GUI.color = Color.red;
+
+        GUI.backgroundColor = Color.black;
+
+        //GUI.skin.label.font = GUI.skin.button.font = GUI.skin.box.font = font;
+        GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = fontSize;
+
+        GUI.depth = renderingDepth;
+
+        //GUI.Label(content, Text);
+        //GUI.Box(wrapper, "");
+
+
+        GUI.skin.box.alignment = TextAnchor.UpperLeft;
+
+        int horizontalOffset = 20;
+        int verticalOffset = 15;
+        GUI.skin.box.padding = new RectOffset(horizontalOffset, horizontalOffset, verticalOffset, verticalOffset);
+        GUI.Box(wrapper, Text, currentStyle);
+        //GUI.Label(content, Text);
+    }
+
     private void OnGUI()
     {
         if (isHovered)
         {
-            if (Text.Length == 0)
-                return;
-
-            InitStyles();
-
-            //GUIStyle guiStyle = new GUIStyle();
-            //Font font = new Font();
-            //font
-            //guiStyle.font = font;
-            //guiStyle.fontSize = 20;
-
-            float mouseX = Input.mousePosition.x;
-            float mouseY = Screen.height - Input.mousePosition.y;
-
-            float offsetX = 15f;
-            float offsetY = 15f;
-
-
-            float contentWidth = 225f;
-            float contentHeight = 225f;
-
-            float rightSideOfScreenOffset = 0;
-
-            if (mouseX > Screen.width  - contentWidth)
-                rightSideOfScreenOffset = contentWidth;
-
-            Rect content = new Rect(mouseX + offsetX - rightSideOfScreenOffset, mouseY, contentWidth, contentHeight);
-            Rect wrapper = new Rect(mouseX - offsetX - rightSideOfScreenOffset, mouseY - offsetY, contentWidth + offsetX, contentHeight + offsetY);
-
-            GUI.color = Color.red;
-
-            GUI.backgroundColor = Color.black;
-
-            //GUI.skin.label.font = GUI.skin.button.font = GUI.skin.box.font = font;
-            GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = fontSize;
-
-            GUI.depth = renderingDepth;
-
-            //GUI.Label(content, Text);
-            //GUI.Box(wrapper, "");
-
-
-            GUI.skin.box.alignment = TextAnchor.UpperLeft;
-
-            int horizontalOffset = 20;
-            int verticalOffset = 15;
-            GUI.skin.box.padding = new RectOffset(horizontalOffset, horizontalOffset, verticalOffset, verticalOffset);
-            GUI.Box(wrapper, Text, currentStyle);
-            //GUI.Label(content, Text);
+            ShowHint();
         }
     }
 
