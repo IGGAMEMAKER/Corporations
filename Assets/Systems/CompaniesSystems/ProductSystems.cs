8fd4a774c50c8eaf2f358dd1ba6c8480bc0f2722
gaga <23i03g@mail.ru>
2019-03-03 15:09:01 +0400

    Made TeamScreen prefab. It will be deleted in future

--- Assets/Systems/CompaniesSystems/ProductSystems.cs
+++ Assets/Systems/CompaniesSystems/ProductSystems.cs
@@ -4,11 +4,7 @@
     {
         Add(new ProductInitializerSystem(contexts));
         Add(new LogProductChangesSystem(contexts));
-        
-        // Adds task when upgrade product button is pressed
-        Add(new ProductUpgradeEventHandler(contexts));
 
-        // updates product data
-        Add(new ProductProcessUpgradeEvent(contexts));
+        Add(new ProductUpgradeSystems(contexts));
     }
 }
--- Assets/Systems/CompaniesSystems/ProductUpgradeSystems.cs
+++ Assets/Systems/CompaniesSystems/ProductUpgradeSystems.cs
@@ -2,6 +2,19 @@
 using Entitas;
 using UnityEngine;
 
+
+public class ProductUpgradeSystems : Feature
+{
+    public ProductUpgradeSystems(Contexts contexts) : base("Product Upgade Systems")
+    {
+        // Adds task when upgrade product button is pressed
+        Add(new ProductUpgradeEventHandler(contexts));
+
+        // updates product data
+        Add(new ProductProcessUpgradeEvent(contexts));
+    }
+}
+
 public class ProductProcessUpgradeEvent : IExecuteSystem
 {
     readonly GameContext _context;
