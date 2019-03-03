4a44082ce9e849970b7650a728e26a22f1aaf2ff
gaga <23i03g@mail.ru>
2019-03-03 20:17:40 +0400

    removed analytics mockup from technology view

--- Assets/ProductInfoView.cs
+++ Assets/ProductInfoView.cs
@@ -1,19 +1,12 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 
 public class ProductInfoView : View
 {
     public Text Level;
-    public ProgressBar ProgressBar;
-
-    public GameObject UpgradeProductProgressBar;
-    public GameObject ExploreProductProgressBar;
 
     void Render()
     {
         AnimateIfValueChanged(Level, myProduct.ProductLevel + "");
-
-        //ProgressBar.enabled = false;
     }
 
     // Update is called once per frame
