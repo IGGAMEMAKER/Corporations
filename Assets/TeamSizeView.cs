b97151ab5fdd51ba7bf59d8d09379866b8986d42
gaga <23i03g@mail.ru>
2019-03-14 14:00:46 +0400

    development screen changes

---
+++ Assets/TeamSizeView.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamSizeView : View
+{
+    public Text Programmers;
+    public Text Managers;
+    public Text Marketers;
+    public Text MaxSize;
+    public Text CurrentSize;
+
+    void Render(TeamComponent team)
+    {
+        Programmers.text = team.Programmers.ToString();
+        Managers.text = team.Managers.ToString();
+        Marketers.text = team.Marketers.ToString();
+
+        MaxSize.text = (team.Managers + 1) * 7 + "";
+        int size = team.Managers + team.Marketers + team.Programmers + 1;
+        CurrentSize.text = size.ToString(); // 1 - CEO
+    }
+
+    void Update()
+    {
+        Render(myProductEntity.team);
+    }
+}
