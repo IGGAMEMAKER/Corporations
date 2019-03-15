b3d30bea079bd830ea275787e5b35845a1aea563
gaga <23i03g@mail.ru>
2019-03-16 01:39:26 +0400

    refactored CooldownView

--- Assets/CooldownView.cs
+++ Assets/CooldownView.cs
@@ -7,11 +7,20 @@ public class CooldownView : View
 {
     public TaskType TaskType;
     Button Target;
+    Color BaseColor;
 
     // Start is called before the first frame update
     void Start()
     {
         Target = GetComponent<Button>();
+        BaseColor = Target.colors.disabledColor;
+    }
+
+    void SetDisabledColor(Color color)
+    {
+        ColorBlock cb = Target.colors;
+        cb.disabledColor = color;
+        Target.colors = cb;
     }
 
     void AnimateButtonBackground(TaskComponent taskComponent)
@@ -19,15 +28,14 @@ public class CooldownView : View
         float part = GetTaskCompletionPercentage(taskComponent) / 100f;
         Color color = new Color(255, 255, 255, 0.2f + 0.3f * part);
 
-        ColorBlock cb = Target.colors;
-        cb.disabledColor = color;
-        Target.colors = cb;
+        SetDisabledColor(color);
         Target.interactable = false;
     }
 
     void ResetButton()
     {
         Target.interactable = true;
+        SetDisabledColor(BaseColor);
     }
 
     void ResetColor()
