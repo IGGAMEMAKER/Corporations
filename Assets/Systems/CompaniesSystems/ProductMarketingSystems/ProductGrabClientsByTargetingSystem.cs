9d1f26b810f9537f19be1894306f1097330956af
gaga <23i03g@mail.ru>
2019-03-17 23:36:25 +0400

    interface changes

--- Assets/Systems/CompaniesSystems/ProductMarketingSystems/ProductGrabClientsByTargetingSystem.cs
+++ Assets/Systems/CompaniesSystems/ProductMarketingSystems/ProductGrabClientsByTargetingSystem.cs
@@ -32,10 +32,6 @@ class ProductGrabClientsByTargetingSystem : ReactiveSystem<GameEntity>
                 e.marketing.Clients += baseForNiche * (100 + brandModifier) / 100;
                 e.product.Resources.Spend(need);
             }
-            //else
-            //{
-            //    e.isTargeting = false;
-            //}
         }
     }
 
