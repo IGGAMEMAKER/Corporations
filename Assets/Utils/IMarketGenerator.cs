a1c2f32c2a7b52bf749cdbbacbdcbc2730c05031
gaga <23i03g@mail.ru>
2019-04-03 01:22:02 +0400

    Added marketGenerator spec

--- Assets/Utils/IMarketGenerator.cs
+++ Assets/Utils/IMarketGenerator.cs
@@ -1,6 +1,26 @@
 ﻿public interface IMarketGenerator
 {
-    //void AttachNicheToIndustry(Niche niche, Industry industry);
+    // Attach niche to industry as independent chain
+    // ----
+    //          | -- niche1
+    // Industry | -- niche
+    //          | -- niche2
+    // ----
+    void AttachNicheToIndustry(Niche niche, Industry industry);
 
+    void ForkNiche(Niche parent, Niche child);
+    
+    // all child niches will try to be the next big thing
+    // this means that clients will move from one niche to another
+    void SetChildsAsCompetingNiches(Niche parent);
 
+    // This means, that this niche will not outgrow sourceNiche by number of clients
+    // dependency specifies percentage of clients,
+    // aka if dependency is 20, max size of this niche will be less than 20% of sourceNiche
+    void SetNicheAsDependant(Niche niche, Niche sourceNiche, int dependency = 100);
+
+    // Helpful for cross promotions
+    // Companies in these niches will be able to exchange their audiences way more effectively
+    // compatibility 0 - 100
+    void SetNichesAsSynergic(Niche niche1, Niche niche2, int compatibility);
 }
