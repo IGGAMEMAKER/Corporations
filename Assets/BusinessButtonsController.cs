c5b7782b6fa3792febd11001cf465202641fc1ca
gaga <23i03g@mail.ru>
2019-04-10 21:11:20 +0400

    Toggling BusinessButtons

---
+++ Assets/BusinessButtonsController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BusinessButtonsController : View
+{
+    public GameObject ManageCompanyButton;
+    public GameObject PromoteToGroupButton;
+    public GameObject PivotButton;
+    public GameObject LeaveCEOButton;
+    public GameObject NominateAsCEO;
+
+    private void OnEnable()
+    {
+        var c = SelectedCompany;
+
+        bool isProductCompany = c.company.CompanyType == CompanyType.ProductCompany;
+        bool isGroupCEOAlready = MyGroupEntity != null;
+
+        // controlled company buttons
+        ManageCompanyButton.SetActive(c.isControlledByPlayer);
+
+        PromoteToGroupButton.SetActive(c.isControlledByPlayer && isProductCompany);
+
+        PivotButton.SetActive(c.isControlledByPlayer && isProductCompany);
+
+        LeaveCEOButton.SetActive(c.isControlledByPlayer);
+
+        // not controlled
+        NominateAsCEO.SetActive(!c.isControlledByPlayer && !isGroupCEOAlready);
+    }
+}
