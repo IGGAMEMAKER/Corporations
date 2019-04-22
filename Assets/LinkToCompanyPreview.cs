32acb892a9b9075be78172071202ae83954f5e7a
gaga <23i03g@mail.ru>
2019-04-22 19:57:43 +0400

    cleaning

--- Assets/LinkToCompanyPreview.cs
+++
@@ -1,9 +0,0 @@
-﻿//public class LinkToCompanyPreview : ButtonController
-//{
-//    public int CompanyId;
-
-//    public override void Execute()
-//    {
-//        NavigateToBusinessScreen(CompanyId);
-//    }
-//}
