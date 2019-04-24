e71b9aee56e1e51166496d32478550b0e128860c
gaga <23i03g@mail.ru>
2019-04-24 19:19:12 +0400

    first string builder usage

--- Assets/CurrentProductPriceView.cs
+++ Assets/CurrentProductPriceView.cs
@@ -1,19 +1,36 @@
 ﻿using Assets.Utils;
+using System.Text;
 
 public class CurrentProductPriceView : ParameterView
     //, IFinanceListener
 {
     public override string RenderHint()
     {
-        if (MyProductEntity.finance.price == 0)
+        if (IsFree)
             return "This product is FREE. Increase prices to start making money!";
 
-        return "Base price: " + CompanyEconomyUtils.GetBaseProductPrice(MyProductEntity)
-            + "$ \n]nWe increased price by" + (MyProductEntity.finance.price - 1) * 10 + "%";
+        int price = (MyProductEntity.finance.price - 1) * 10;
+
+        StringBuilder text = new StringBuilder();
+
+        text.AppendFormat("Base price: {0}$ \n\nWe increased price by {1}%", CompanyEconomyUtils.GetBaseProductPrice(MyProductEntity), price);
+
+        return text.ToString();
+    }
+
+    bool IsFree
+    {
+        get
+        {
+            return MyProductEntity.finance.price == 0;
+        }
     }
 
     public override string RenderValue()
     {
+        if (IsFree)
+            return "Free";
+
         return CompanyEconomyUtils.GetProductPrice(MyProductEntity) + "$";
     }
 }
