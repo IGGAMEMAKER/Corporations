2c84fa8d38204c3a79bb873c09e2f31d236d4c45
gaga <23i03g@mail.ru>
2019-04-29 01:16:35 +0400

    removed cube which added 30% GPU usage

--- Assets/ApplicationSettings.cs
+++ Assets/ApplicationSettings.cs
@@ -7,6 +7,6 @@ public class ApplicationSettings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 60;
     }
 }
