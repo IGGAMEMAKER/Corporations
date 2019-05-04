72d0e289e939bc668abdaf969e1de4d0d2e4c0d3
gaga <23i03g@mail.ru>
2019-05-04 05:43:34 +0400

    moved scripts to Loading scene. Cleaned GameController script

--- Assets/Singletons/GameController.cs
+++ Assets/Singletons/GameController.cs
@@ -2,48 +2,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Services
-{
-    public readonly IViewService View;
-    //public readonly IApplicationService Application;
-    //public readonly ITimeService Time;
-    //public readonly IInputService Input;
-    //public readonly IAiService Ai;
-    //public readonly IConfigurationService Config;
-    //public readonly ICameraService Camera;
-    //public readonly IPhysicsService Physics;
-
-    public Services(IViewService view)
-        //, IApplicationService application, ITimeService time, IInputService input, IAiService ai, IConfigurationService config, ICameraService camera, IPhysicsService physics)
-    {
-        View = view;
-        //Application = application;
-        //Time = time;
-        //Input = input;
-        //Ai = ai;
-        //Config = config;
-        //Camera = camera;
-        //Physics = physics;
-    }
-}
-
 public class GameController : MonoBehaviour
 {
     Systems _systems;
-
-    public Canvas Canvas;
-    public GameObject Gameplay;
-
-    void SetViewsActivation(bool show)
-    {
-        Canvas.gameObject.SetActive(show);
-        Gameplay.SetActive(show);
-    }
-
+    
     void Start()
     {
-        Debug.Log("GameController starts systems...");
-
         // get a reference to the contexts
         var contexts = Contexts.sharedInstance;
 
@@ -51,17 +15,12 @@ public class GameController : MonoBehaviour
         _systems = new Feature("Systems")
             .Add(new MainSystem(contexts));
 
-        //var _services = new Services(
-        //    new UnityViewService() // responsible for creating gameobjects for views
-        //);
-
         // call Initialize() on all of the IInitializeSystems
         _systems.Initialize();
 
-        //SceneManager.LoadSceneAsync(1);
+        //DontDestroyOnLoad(gameObject);
 
-        Debug.Log("Activate everything");
-        SetViewsActivation(true);
+        //SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
     }
 
     void Update()
