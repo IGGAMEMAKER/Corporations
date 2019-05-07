411c4c5885184c908d234736cfd984a7a5da2908
gaga <23i03g@mail.ru>
2019-05-07 22:40:49 +0400

    Investment Goal mockup

---
+++ Assets/InvestmentGoalView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvestmentGoalView : MonoBehaviour
+{
+    public Text Importance;
+    public Text Goal;
+    public ProgressBar ProgressBar;
+
+    void Start()
+    {
+        Render();
+    }
+
+    void Render()
+    {
+        Importance.text = "58%";
+        Goal.text = VisualFormattingUtils.Colorize("Become market fit", VisualConstants.COLOR_NEGATIVE);
+        ProgressBar.SetValue(7, 8);
+    }
+}
