f5fc68068b0f6d999639d298d3ec889c04ab6388
gaga <23i03g@mail.ru>
2019-05-13 21:14:57 +0400

    Update ProductLevelView.cs

--- Assets/DevelopmentScreen/ProductLevelView.cs
+++ Assets/DevelopmentScreen/ProductLevelView.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine.UI;
 
 public class ProductLevelView : View
+    , IAnyDateListener
 {
     Text Level;
 
     private void Start()
     {
         Level = GetComponent<Text>();
+
+        ListenDateChanges(this);
     }
 
-    void Update()
+    void OnEnable()
     {
+        // TODO Update
+
         Render();
     }
 
     void Render()
     {
         AnimateIfValueChanged(Level, MyProduct.ProductLevel + " LVL");
+    }
+
+    void IAnyDateListener.OnAnyDate(GameEntity entity, int date)
+    {
+        Render();
     }
 }
