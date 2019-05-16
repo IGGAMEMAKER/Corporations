b99aea15fd5fe4be5b344c8355d70a6f27b00c7f
gaga <23i03g@mail.ru>
2019-05-16 21:10:57 +0400

    product company income into separate file

--- Assets/Utils/Economy/ProductCompanyEconomics.cs
+++ Assets/Utils/Economy/ProductCompanyEconomics.cs
@@ -28,58 +28,6 @@ namespace Assets.Utils
             return MarketingUtils.GetClients(c) * 100;
         }
 
-        public static float GetBaseProductPrice(GameEntity e, GameContext context)
-        {
-            return e.finance.basePrice;
-        }
-
-        public static float GetProductPrice(GameEntity e, GameContext context)
-        {
-            if (e.finance.price == Pricing.Free) return 0;
-
-            float price = (float)e.finance.price;
-
-            return GetBaseProductPrice(e, context) * price / 100;
-        }
-
-        private static long GetProductCompanyIncome(GameEntity e, GameContext context)
-        {
-            float income = 0;
-
-            foreach (var pair in e.marketing.Segments)
-                income += GetIncomeBySegment(context, e.company.Id, pair.Key);
-
-            return Convert.ToInt64(income);
-        }
-
-        internal static float GetSegmentPrice(GameContext gameContext, int companyId, UserType userType)
-        {
-            float modifier = 0;
-
-            switch (userType)
-            {
-                case UserType.Newbie: modifier = 0.1f; break;
-                case UserType.Regular: modifier = 0.6f; break;
-                case UserType.Core: modifier = 2f; break;
-            }
-
-            var c = CompanyUtils.GetCompanyById(gameContext, companyId);
-
-            return modifier * GetBaseProductPrice(c, gameContext);
-        }
-
-        internal static long GetIncomeBySegment(GameContext gameContext, int companyId, UserType userType)
-        {
-            var c = CompanyUtils.GetCompanyById(gameContext, companyId);
-
-            long clients = c.marketing.Segments[userType];
-
-            float price = GetSegmentPrice(gameContext, companyId, userType) * GetProductPrice(c, gameContext);
-
-            return clients * Convert.ToInt64(price);
-        }
-
-
         private static string GetProductCompanyIncomeDescription(GameEntity gameEntity, GameContext gameContext)
         {
             return $"Income of this company equals {GetProductCompanyIncome(gameEntity, gameContext)}";
---
+++ Assets/Utils/Economy/ProductCompanyIncome.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Assets.Utils
+{
+    static partial class CompanyEconomyUtils
+    {
+        private static long GetProductCompanyIncome(GameEntity e, GameContext context)
+        {
+            float income = 0;
+
+            foreach (var pair in e.marketing.Segments)
+                income += GetIncomeBySegment(context, e.company.Id, pair.Key);
+
+            return Convert.ToInt64(income);
+        }
+
+        internal static long GetIncomeBySegment(GameContext gameContext, int companyId, UserType userType)
+        {
+            var c = CompanyUtils.GetCompanyById(gameContext, companyId);
+
+            long clients = c.marketing.Segments[userType];
+
+            float price = GetSegmentPrice(gameContext, companyId, userType);
+
+            return clients * Convert.ToInt64(price);
+        }
+
+        internal static float GetSegmentPrice(GameContext gameContext, int companyId, UserType userType)
+        {
+            var c = CompanyUtils.GetCompanyById(gameContext, companyId);
+
+            return GetUserTypePriceModifier(userType) * GetProductPrice(c, gameContext);
+        }
+
+        public static float GetProductPrice(GameEntity e, GameContext context)
+        {
+            if (e.finance.price == Pricing.Free) return 0;
+
+            float price = (float)e.finance.price;
+
+            return GetBaseProductPrice(e, context) * price / 100;
+        }
+
+        public static float GetBaseProductPrice(GameEntity e, GameContext context)
+        {
+            return e.finance.basePrice;
+        }
+
+        private static float GetUserTypePriceModifier(UserType userType)
+        {
+            switch (userType)
+            {
+                case UserType.Newbie: return 0.1f;
+                case UserType.Regular: return 0.6f;
+                case UserType.Core: return 2f;
+
+                default: return 0.1f;
+            }
+        }
+    }
+}
