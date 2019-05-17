d90cb23cef2492689cbb925a957f7ebde628ae38
gaga <23i03g@mail.ru>
2019-05-17 15:13:28 +0400

    Update InvestmentDescriptions.cs

--- Assets/Utils/Inevestments/InvestmentDescriptions.cs
+++ Assets/Utils/Inevestments/InvestmentDescriptions.cs
@@ -6,18 +6,19 @@
         {
             int opinion = GetInvestorOpinion(gameContext, company, investor);
 
-            string title = VisualUtils.Describe(
-                "They will invest in this company if asked",
-                "They will not invest",
-                opinion
-            );
-
             var description = new BonusContainer("Investor opinion", opinion);
 
             if (company.hasProduct)
                 description.Append("Product competitiveness", NicheUtils.GetProductCompetitivenessBonus(company));
 
             description.Append("Same goals", 25);
+
+
+            string title = VisualUtils.Describe(
+                "They will invest in this company if asked",
+                "They will not invest",
+                opinion
+            );
 
             return title + "\n" + description.ToString();
         }
@@ -37,18 +38,6 @@
 
                 case InvestorGoal.IPO:
                     return "IPO";
-
-                //case InvestorGoal.GrowProfit:
-                //    return "Grow profit by " + Constants.INVESTMENT_GOAL_GROWTH_REQUIREMENT_PROFIT_GROWTH + "%";
-
-                //case InvestorGoal.BecomeBestByTech:
-                //    return "Become technology leader";
-
-                //case InvestorGoal.GrowClientBase:
-                //    return "Grow client base";
-
-                //case InvestorGoal.ProceedToNextRound:
-                //    return "Proceed to next investment round";
                 default:
                     return investorGoal.ToString();
             }
