fe11e78d8a14c334e2eb80da4452a61c5ff255b0
gaga <23i03g@mail.ru>
2019-05-19 01:51:47 +0400

    Update Controller.cs

--- Assets/BaseClasses/Controller.cs
+++ Assets/BaseClasses/Controller.cs
@@ -17,6 +17,7 @@ public abstract class Controller : BaseClass
         ScheduleUtils.ListenDateChanges(GameContext, dateListener);
     }
 
+
     public void Render()
     {
         foreach (var view in GetComponents<View>())
