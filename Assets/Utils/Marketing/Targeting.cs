eac11d68ea1ffb9d8c26dd38904b2f202d31ff80
gaga <23i03g@mail.ru>
2019-05-18 23:40:38 +0400

    Update Targeting.cs

--- Assets/Utils/Marketing/Targeting.cs
+++ Assets/Utils/Marketing/Targeting.cs
@@ -9,7 +9,47 @@ namespace Assets.Utils
             long adCost = 700;
             // TODO Calculate proper base value!
 
-            return new TeamResource(0, 0, 0, 0, adCost);
+            var c = CompanyUtils.GetCompanyById(gameContext, companyId);
+
+            var financing = GetMarketingFinancingPriceModifier(c.finance.marketingFinancing);
+
+            return new TeamResource(0, 0, 0, 0, adCost * financing);
+        }
+
+        public static int GetMarketingFinancingPriceModifier(MarketingFinancing financing)
+        {
+            switch (financing)
+            {
+                case MarketingFinancing.Low: return 1;
+                case MarketingFinancing.Medium: return 4;
+                case MarketingFinancing.High: return 9;
+
+                default: return 0;
+            }
+        }
+
+        public static int GetMarketingFinancingAudienceReachModifier(MarketingFinancing financing)
+        {
+            switch (financing)
+            {
+                case MarketingFinancing.Low: return 1;
+                case MarketingFinancing.Medium: return 3;
+                case MarketingFinancing.High: return 7;
+
+                default: return 0;
+            }
+        }
+
+        public static int GetMarketingFinancingBrandPowerGainModifier(MarketingFinancing financing)
+        {
+            switch (financing)
+            {
+                case MarketingFinancing.Low: return 0;
+                case MarketingFinancing.Medium: return 1;
+                case MarketingFinancing.High: return 4;
+
+                default: return 0;
+            }
         }
 
         public static long GetTargetingEffeciency(GameContext gameContext, GameEntity e)
@@ -17,8 +57,9 @@ namespace Assets.Utils
             long baseForNiche = 100;
 
             long brandModifier = e.marketing.BrandPower / 2;
+            long audienceReachModifier = GetMarketingFinancingAudienceReachModifier(e.finance.marketingFinancing);
 
-            return baseForNiche * (100 + brandModifier) / 100;
+            return baseForNiche * audienceReachModifier * (100 + brandModifier) / 100;
         }
     }
 }
