f89c2e12ea25d500114d42183945e8c382e7f33a
gaga <23i03g@mail.ru>
2019-05-25 21:57:26 +0400

    mocking investments

--- Assets/FillSharesList.cs
+++ Assets/FillSharesList.cs
@@ -1,8 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FillSharesList : MonoBehaviour
+public class FillSharesList : View
 {
+    public override void ViewRender()
+    {
+        base.ViewRender();
 
+        GetComponent<ShareholdersListView>().SetItems(GetInvestments());
+    }
+
+    private GameEntity[] GetInvestments()
+    {
+        return new GameEntity[0];
+    }
 }
