e00149d59a27a10e334f23396b12cab58d2a9176
gaga <23i03g@mail.ru>
2019-06-03 19:46:51 +0400

    Update ListenProductReleaseChanges.cs

--- Assets/ListenProductReleaseChanges.cs
+++ Assets/ListenProductReleaseChanges.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ListenProductReleaseChanges : Controller
+﻿public class ListenProductReleaseChanges : Controller
     , IReleaseListener
 {
     public override void AttachListeners()
