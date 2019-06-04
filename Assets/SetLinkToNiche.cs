d433aa3ca13b0fbbae23944aafcc896852165a61
gaga <23i03g@mail.ru>
2019-06-04 21:42:35 +0400

    link to niche and marketing screens

---
+++ Assets/SetLinkToNiche.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Utils;
+using Assets.Utils.Formatting;
+using UnityEngine.UI;
+
+public class SetLinkToNiche : View
+{
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var niche = MyProductEntity.product.Niche;
+
+        GetComponent<LinkToNiche>().SetNiche(niche);
+        GetComponent<Text>().text = Visuals.Link(EnumUtils.GetFormattedNicheName(niche));
+    }
+}
