69e601d77e9ebe682358a842e9c753d2080405a6
gaga <23i03g@mail.ru>
2019-06-07 16:25:54 +0400

    refactoring and removing useless files

--- Assets/Systems/AI/AIProductSystems/Strategies/TakeLeadership.cs
+++ Assets/Systems/AI/AIProductSystems/Strategies/TakeLeadership.cs
@@ -20,7 +20,6 @@ public partial class AIProductSystems : OnDateChange
 
     void FocusOnIdeas(GameEntity product)
     {
-
         ProductDevelopmentUtils.ToggleDevelopment(gameContext, product.company.Id, DevelopmentFocus.Concept);
     }
 
