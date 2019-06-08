fc0c48e600e52588d5487992146c121e51e9123b
gaga <23i03g@mail.ru>
2019-06-09 00:59:45 +0400

    Update FixLoyalty.cs

--- Assets/Systems/AI/AIProductSystems/Strategies/FixLoyalty.cs
+++ Assets/Systems/AI/AIProductSystems/Strategies/FixLoyalty.cs
@@ -15,15 +15,38 @@ public partial class AIProductSystems : OnDateChange
         //  // focus on ideas?
     }
 
+    long GetSegmentImportance(GameEntity product, UserType userType)
+    {
+        var loyalty = MarketingUtils.GetClientLoyalty(gameContext, product.company.Id, userType);
+
+        var income = CompanyEconomyUtils.GetIncomeBySegment(gameContext, product.company.Id, userType);
+        var totalIncome = CompanyEconomyUtils.GetCompanyIncome(product, gameContext);
+
+        // TODO 1 DISLOYAL CORE USER IS WAY MORE IMPORTANT THAN THOUSANDS OF PAYING REGULAR USERS
+        // WHICH IS NOT ALWAYS WORTH IT
+        var badLoyaltyPenalty = loyalty < 0 ? 500 : 0;
+        var incomeBonus = (long)income * 100 / totalIncome;
+
+        // TODO COUNT COMPANY GOAL TOO!
+        return badLoyaltyPenalty + incomeBonus;
+    }
+
+    UserType GetMoreImportantSegment(GameEntity product)
+    {
+        var CorePriority = 5 + GetSegmentImportance(product, UserType.Core);
+        var RegularPriority = 4 + GetSegmentImportance(product, UserType.Regular);
+
+        return CorePriority > RegularPriority ? UserType.Core : UserType.Regular;
+    }
+
     void ImproveSegments(GameEntity product)
     {
         var core = MarketingUtils.GetClientLoyalty(gameContext, product.company.Id, UserType.Core);
         var regular = MarketingUtils.GetClientLoyalty(gameContext, product.company.Id, UserType.Regular);
 
-        if (core < regular)
-            UpgradeSegment(product, UserType.Core);
-        else
-            UpgradeSegment(product, UserType.Regular);
+        var segment = GetMoreImportantSegment(product);
+
+        UpgradeSegment(product, segment);
     }
 
     void UpgradeSegment(GameEntity product, UserType userType)
