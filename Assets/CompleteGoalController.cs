d83bb0e55304fa32037d4facfbe2b63757381db6
gaga <23i03g@mail.ru>
2019-06-13 16:05:27 +0400

    Update Tutorial system

--- Assets/CompleteGoalController.cs
+++ Assets/CompleteGoalController.cs
@@ -4,8 +4,17 @@ public class CompleteGoalController : ButtonController
 {
     public override void Execute()
     {
+        var goal = MyProductEntity.companyGoal.InvestorGoal;
+
         InvestmentUtils.CompleteGoal(MyProductEntity, GameContext);
 
-        UnlockTutorialFunctionality(TutorialFunctionality.CompletedFirstGoal);
+        switch (goal)
+        {
+            case InvestorGoal.Prototype: UnlockTutorialFunctionality(TutorialFunctionality.CompletedFirstGoal); break;
+            case InvestorGoal.FirstUsers: UnlockTutorialFunctionality(TutorialFunctionality.GoalFirstUsers); break;
+            case InvestorGoal.BecomeMarketFit: UnlockTutorialFunctionality(TutorialFunctionality.GoalBecomeMarketFit); break;
+            case InvestorGoal.BecomeProfitable: UnlockTutorialFunctionality(TutorialFunctionality.GoalBecomeProfitable); break;
+            case InvestorGoal.Release: UnlockTutorialFunctionality(TutorialFunctionality.GoalRelease); break;
+        }
     }
 }
