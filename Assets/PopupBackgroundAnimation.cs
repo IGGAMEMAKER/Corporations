062beac47807117a529fd05f3054f92d329c2db6
gaga <23i03g@mail.ru>
2019-06-14 17:23:53 +0400

    PopupBackgroundAnimation

---
+++ Assets/PopupBackgroundAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupBackgroundAnimation : MonoBehaviour
+{
+    float animationLength = 0.8f;
+    float duration;
+
+    Image Image;
+
+    Color color;
+
+    void Start ()
+    {
+        Image = GetComponent<Image>();
+        duration = 0;
+
+        color = new Color(0, 0, 0, 1);
+    }
+
+    void Render()
+    {
+        color.a = 210f * duration / 256f / animationLength;
+
+        Image.color = color;
+
+        duration += Time.deltaTime;
+
+        if (duration >= animationLength)
+            Destroy(this);
+    }
+
+    void Update()
+    {
+        Render();
+    }
+}
