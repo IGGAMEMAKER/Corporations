89ad216a2aa6606ca6437823af4f64bc81c18b98
gaga <23i03g@mail.ru>
2019-06-15 23:11:32 +0400

    Update LazyUpdate.cs

--- Assets/BaseClasses/LazyUpdate.cs
+++ Assets/BaseClasses/LazyUpdate.cs
@@ -1,17 +1,27 @@
-﻿public class LazyUpdate : Controller
+﻿using System.Collections.Generic;
+
+public class LazyUpdate : Controller
     , IAnyDateListener
+    , IMenuListener
 {
     public override void AttachListeners()
     {
         ListenDateChanges(this);
+        ListenMenuChanges(this);
     }
 
     public override void DetachListeners()
     {
         UnListenDateChanges(this);
+        UnListenMenuChanges(this);
     }
 
     void IAnyDateListener.OnAnyDate(GameEntity entity, int date)
+    {
+        Render();
+    }
+
+    void IMenuListener.OnMenu(GameEntity entity, ScreenMode screenMode, Dictionary<string, object> data)
     {
         Render();
     }
