7212892d410092677f1dedccbd24fb7b98cbfcb2
gaga <23i03g@mail.ru>
2019-06-16 22:21:08 +0400

    Update ProductCompanyCompetingPreview.cs

--- Assets/ProductCompanyCompetingPreview.cs
+++ Assets/ProductCompanyCompetingPreview.cs
@@ -74,6 +74,6 @@ public class ProductCompanyCompetingPreview : View,
     void IProductListener.OnProduct(GameEntity entity, int id, NicheType niche, int productLevel, Dictionary<UserType, int> segments)
     {
         //if (CurrentScreen == ScreenMode.NicheScreen)
-        RenderProductInfo(name, productLevel);
+        RenderProductInfo(entity.company.Name, productLevel);
     }
 }
