f8c5041bd1a5fa6f9e01b51d30e3068a76b1885f
gaga <23i03g@mail.ru>
2019-06-18 20:38:01 +0400

    Test campaign resources checker

---
+++ Assets/CheckTestCampaignResources.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Classes;
+using Assets.Utils;
+
+public class CheckTestCampaignResources : ResourceChecker
+{
+    public override string GetBaseHint()
+    {
+        return "Test campaign gives you a lot of feedback (ideas) and small amount of Core Users";
+    }
+
+    public override TeamResource GetRequiredResources()
+    {
+        return MarketingUtils.GetTestCampaignCost(GameContext, MyProductEntity);
+    }
+}
