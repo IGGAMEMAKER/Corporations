ccdb59b8c4ae1eb48790c55de521a8d0ac5b2c50
gaga <23i03g@mail.ru>
2019-06-19 18:40:27 +0400

    render segment improvement level instead of loyalty

--- Assets/ClientSegmentPreview.cs
+++ Assets/ClientSegmentPreview.cs
@@ -8,6 +8,7 @@ public class ClientSegmentPreview : View
     public Text UserTypeLabel;
 
     public ColoredValuePositiveOrNegative LoyaltyLabel;
+    public ColoredValueGradient SegmentImprovements;
     public Text Income;
     public Text IncomeLabel;
 
@@ -51,13 +52,35 @@ public class ClientSegmentPreview : View
 
         RenderSegmentIncome(CompanyId, UserType);
 
-        RenderLoyalty(CompanyId, UserType);
+        //RenderLoyalty(CompanyId, UserType);
+        RenderSegmentLevel(UserType, c);
 
+        RenderUpdateSegmentButton();
+    }
+
+    void RenderUpdateSegmentButton()
+    {
         UpdateSegmentController.SetSegment(UserType);
         UpdateSegmentController.gameObject.GetComponent<CheckSegmentImprovementResources>().SetSegment(UserType);
 
         bool isInnovation = ProductUtils.IsWillInnovate(MyProductEntity, GameContext, UserType);
         UpdateSegmentController.gameObject.GetComponentInChildren<Text>().text = isInnovation ? "Make an innovation!" : "Copy competitors";
+    }
+
+    void RenderSegmentLevel(UserType userType, GameEntity company)
+    {
+        SegmentImprovements.UpdateValue(company.product.Segments[userType]);
+
+        var demand = ProductUtils.GetSegmentMarketDemand(company, GameContext, userType);
+
+        SegmentImprovements.minValue = demand - 7;
+        SegmentImprovements.maxValue = demand;
+
+        var apps = String.Join("\n", NicheUtils.GetCompetitorSegmentLevels(MyProductEntity, GameContext, userType));
+
+        var hint = $"Best apps for {EnumUtils.GetFormattedUserType(userType)}\n\n{apps}";
+
+        LoyaltyHint.SetHint(hint);
     }
 
     void RenderAudience(UserType userType, GameEntity c)
@@ -78,12 +101,12 @@ public class ClientSegmentPreview : View
 
     void RenderLoyalty(int companyId, UserType userType)
     {
-        LoyaltyLabel.UpdateValue(MarketingUtils.GetClientLoyalty(GameContext, CompanyId, UserType));
+        //LoyaltyLabel.UpdateValue(MarketingUtils.GetClientLoyalty(GameContext, CompanyId, UserType));
 
         var hint = MarketingUtils.GetClientLoyaltyDescription(GameContext, companyId, userType);
+        
+        //hint += "\n" + String.Join("\n", NicheUtils.GetCompetitorSegmentLevels(MyProductEntity, GameContext, userType));
 
-        hint += "\n" + String.Join("\n", NicheUtils.GetCompetitorSegmentLevels(MyProductEntity, GameContext, userType));
-
-        LoyaltyHint.SetHint(hint);
+        //LoyaltyHint.SetHint(hint);
     }
 }
--- Assets/Utils/Products/ProductUtils.cs
+++ Assets/Utils/Products/ProductUtils.cs
@@ -23,6 +23,13 @@ namespace Assets.Utils
             return new TeamResource(costs.TechCost * innovationModifier, 0, 0, costs.IdeaCost * innovationModifier, 0);
         }
 
+        public static int GetSegmentMarketDemand(GameEntity product, GameContext gameContext, UserType userType)
+        {
+            var niche = NicheUtils.GetNicheEntity(gameContext, product.product.Niche);
+
+            return niche.segment.Segments[userType];
+        }
+
         public static bool IsWillInnovate(GameEntity product, GameContext gameContext, UserType userType)
         {
             var niche = NicheUtils.GetNicheEntity(gameContext, product.product.Niche);
