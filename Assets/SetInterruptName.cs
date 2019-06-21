5cd99feae9634041c15dafdbf196d3209e335df2
gaga <23i03g@mail.ru>
2019-06-21 15:45:43 +0400

    interrupts intro

---
+++ Assets/SetInterruptName.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetInterruptName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Hint>().SetHint("You " + gameObject.name);
+    }
+}
