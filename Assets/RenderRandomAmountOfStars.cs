89fd2d10e40bd151753248c0769cefbe1c0a71c0
gaga <23i03g@mail.ru>
2019-06-23 13:44:32 +0400

    random amount of stars in market screen

---
+++ Assets/RenderRandomAmountOfStars.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RenderRandomAmountOfStars : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        int amountOfStars = Random.Range(1, 6);
+
+        GetComponent<SetAmountOfStars>().SetStars(amountOfStars);
+    }
+}
