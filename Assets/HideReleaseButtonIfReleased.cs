5721c4c55219c67c5f7955ed5b9e69f67efc68d5
gaga <23i03g@mail.ru>
2019-06-23 09:09:21 +0400

    Hide release campaign when released

---
+++ Assets/HideReleaseButtonIfReleased.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HideReleaseButtonIfReleased : View
+{
+    public GameObject ReleaseCampaign;
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        if (MyProductEntity.isRelease)
+            ReleaseCampaign.SetActive(false);
+    }
+}
