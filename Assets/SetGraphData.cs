4bf67acf32aa65b7179b94b2141aae669e51aead
gaga <23i03g@mail.ru>
2019-06-24 22:27:32 +0400

    Graph prefab initialized. Updated Links. Updated GroupManagementScreen

---
+++ Assets/SetGraphData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGraphData : MonoBehaviour
+{
+    public GameObject DotPrefab;
+
+    public List<GameObject> Dots;
+
+    public List<int> XList;
+    public List<int> Values;
+
+    public void SetData()
+    {
+
+    }
+}
