829382225c9e0e8d8118010f449ace848dcbf63e
gaga <23i03g@mail.ru>
2019-06-25 00:33:11 +0400

    trying to make marketing financing for AI

--- Assets/Systems/AI/AIProductSystems/CompanyGoals/GrowCompanyCost/ManageProductCompany.cs
+++ Assets/Systems/AI/AIProductSystems/CompanyGoals/GrowCompanyCost/ManageProductCompany.cs
@@ -51,8 +51,41 @@ public partial class AIProductSystems : OnDateChange
         // 
     }
 
+    // TODO WHAT THE FUUUCK
+    // IT DOES NO CALCULATIONS
+    // JUST GREEDY APPROACH
+    bool TryFinancing(MarketingFinancing marketingFinancing, GameEntity company, long balance, long maintenance)
+    {
+        MarketingUtils.SetFinancing(gameContext, company.company.Id, marketingFinancing);
+
+        var cost = MarketingUtils.GetTargetingCost(gameContext, company.company.Id).money;
+
+        return balance - maintenance - cost > 0;
+    }
+
+    // TODO WHAT THE FUUUCK
+    // IT DOES NO CALCULATIONS
+    // JUST GREEDY APPROACH
+    void SetMarketingFinancingLevel(GameEntity company)
+    {
+        var balance = company.companyResource.Resources.money;
+
+        var maintenance = CompanyEconomyUtils.GetCompanyMaintenance(company, gameContext);
+
+        if (TryFinancing(MarketingFinancing.High, company, balance, maintenance))
+            return;
+
+        if (TryFinancing(MarketingFinancing.Medium, company, balance, maintenance))
+            return;
+
+        if (TryFinancing(MarketingFinancing.Low, company, balance, maintenance))
+            return;
+    }
+
     void ManageCompanyMarketing(GameEntity company)
     {
+        SetMarketingFinancingLevel(company);
+
         StartTargetingCampaign(company);
 
         GrabTestClients(company);
