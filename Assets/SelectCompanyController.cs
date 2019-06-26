8a39734ab81b5aee340232498847be0b7604ac66
gaga <23i03g@mail.ru>
2019-06-26 20:09:39 +0400

    select company on hover in manage companies screen

--- Assets/SelectCompanyController.cs
+++ Assets/SelectCompanyController.cs
@@ -1,9 +1,16 @@
-﻿public class SelectCompanyController : ButtonController
+﻿using UnityEngine.EventSystems;
+
+public class SelectCompanyController : ButtonController, IPointerEnterHandler
 {
     public int companyId;
 
     public override void Execute()
     {
         SetSelectedCompany(companyId);
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        Execute();
     }
 }
