1d2425318800c6d3d4082d861236e6bc5da731bd
gaga <23i03g@mail.ru>
2019-06-27 21:16:23 +0400

    Company research screen

---
+++ Assets/CompanySearchListView.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanySearchListView : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        throw new System.NotImplementedException();
+    }
+}
