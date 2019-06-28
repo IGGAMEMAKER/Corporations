ad93dd62b89e2864e7b4a6d3cd2b9ba38e6b1af1
gaga <23i03g@mail.ru>
2019-06-28 23:53:11 +0400

    Update CompanyTableView.cs

--- Assets/CompanyTableView.cs
+++ Assets/CompanyTableView.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using Assets.Utils;
+using UnityEngine;
 
 public class CompanyTableView : View, IPointerEnterHandler
 {
     public Text CompanyName;
     public SetAmountOfStars SetAmountOfStars;
+    [SerializeField] Text Cost;
+    [SerializeField] Text AudienceGrowth;
+    [SerializeField] Text ValuationGrowth;
     public Image Panel;
 
     GameEntity entity;
@@ -13,13 +17,41 @@ public class CompanyTableView : View, IPointerEnterHandler
     public void SetEntity(GameEntity company)
     {
         entity = company;
-        CompanyName.text = company.company.Name;
+    }
+
+    void Render()
+    {
+        CompanyName.text = entity.company.Name;
 
         SetPanelColor();
+
+        Cost.text = "$" + Format.MinifyToInteger(CompanyEconomyUtils.GetCompanyCost(GameContext, entity.company.Id));
+
+        var audienceGrowth = GetAudienceGrowth(entity);
+        //var valuationGrowth = GetValuationGrowth(entity);
+
+        AudienceGrowth.text = GetAudienceGrowth(entity).ToString();
 
         var rating = CompanyEconomyUtils.GetCompanyRating(GameContext, entity.company.Id);
         SetAmountOfStars.SetStars(rating);
     }
+
+    long GetAudienceGrowth(GameEntity e)
+    {
+        var metrics = e.metricsHistory.Metrics;
+
+        if (metrics.Count < 3)
+            return 0;
+
+        var len = metrics.Count;
+
+        var was =  
+    }
+
+    //long GetValuationGrowth(GameEntity e)
+    //{
+
+    //}
 
     void SetPanelColor()
     {
