a0af676e0c7b4ada4e1707f3979c8d3732f98caf
gaga <23i03g@mail.ru>
2019-07-01 22:03:33 +0400

    filling leaderboard screen

---
+++ Assets/HumanCapitalsTableView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanCapitalsTableView : View
+{
+    GameEntity entity;
+
+    public void SetEntity(GameEntity e)
+    {
+
+    }
+}
---
+++ Assets/PeopleLeaderboardListView.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Utils;
+using Entitas;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleLeaderboardListView : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        var e = entity as GameEntity;
+
+        t.GetComponent<HumanCapitalsTableView>().SetEntity(e);
+    }
+
+    private void OnEnable()
+    {
+        Render();
+    }
+
+    public void Render()
+    {
+        var people = GetInfluencialPeople(GameContext);
+
+        SetItems(people);
+    }
+
+    GameEntity[] GetInfluencialPeople(GameContext gameContext)
+    {
+        var investors = gameContext.GetEntities(GameMatcher.AllOf(GameMatcher.Shareholder, GameMatcher.Human));
+
+        return investors;
+    }
+}
