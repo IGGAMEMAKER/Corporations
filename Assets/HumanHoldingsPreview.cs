8dc8a24c4e4d04a74cde941e343458846b20d7c3
gaga <23i03g@mail.ru>
2019-07-02 02:40:28 +0400

    Update HumanHoldingsPreview.cs

--- Assets/HumanHoldingsPreview.cs
+++ Assets/HumanHoldingsPreview.cs
@@ -6,35 +6,16 @@ using UnityEngine.UI;
 
 public class HumanHoldingsPreview : View
 {
-    int ShareholderId
-    {
-        get
-        {
-            return SelectedHuman.shareholder.Id;
-        }
-    }
-
-    GameEntity company;
-
-    int TotalShares;
-
     public Text NameLabel;
     public Text SharesLabel;
     public Hint SharesAmountHint;
 
     public LinkToProjectView LinkToCompanyPreview;
 
-    GameEntity ShareholderEntity
-    {
-        get
-        {
-            return CompanyUtils.GetInvestorById(GameContext, ShareholderId);
-        }
-    }
+    GameEntity company;
 
     public void SetEntity(GameEntity company)
     {
-        //int shareholderId, BlockOfShares shares
         this.company = company;
 
         Render();
@@ -48,7 +29,7 @@ public class HumanHoldingsPreview : View
     void Render()
     {
         NameLabel.text = company.company.Name;
-        SharesLabel.text = CompanyUtils.GetShareSize(GameContext, company.company.Id, ShareholderId) + "%";
+        SharesLabel.text = CompanyUtils.GetShareSize(GameContext, company.company.Id, SelectedHuman.shareholder.Id) + "%";
 
         SharesAmountHint.SetHint("");
 
