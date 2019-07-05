7b65e6c0c24dfb63ae4206e390bf1c7b0fd1c7bb
gaga <23i03g@mail.ru>
2019-07-05 20:18:31 +0400

    made MainNews prefab

--- Assets/PickMostImportantMessage.cs
+++ Assets/PickMostImportantMessage.cs
@@ -3,15 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickMostImportantMessage : View
+public class PickMostImportantMessage : ListView
 {
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        var notification = entity as NotificationMessage;
+
+        t.GetComponent<NotificationView>()
+            .SetMessage(notification);
+    }
+
     public override void ViewRender()
     {
         base.ViewRender();
 
         var notifications = NotificationUtils.GetNotifications(GameContext);
 
-        var c = GetComponent<NotificationView>();
-        c.SetMessage(notifications[notifications.Count - 1]);
+        var notification = notifications[notifications.Count - 1];
+
+        NotificationMessage[] notificationMessages = new NotificationMessage[] { notification };
+
+        SetItems(notificationMessages);
     }
 }
