923b51e75b4e5f1df6f6cda14696202aec8faa37
gaga <23i03g@mail.ru>
2019-07-07 19:19:04 +0400

    added Desire to Sell file

--- Assets/Utils/Companies/DesireToBuyOrSell/DesireToBuy.cs
+++ Assets/Utils/Companies/DesireToBuyOrSell/DesireToBuy.cs
@@ -11,6 +11,26 @@ namespace Assets.Utils
             return 1;
         }
 
+        public static long GetStartupAttractiveness(GameContext gameContext, GameEntity startup)
+        {
+            long score = 0;
+
+            if (NicheUtils.IsPerspectiveNiche(gameContext, startup.product.Niche))
+                score += 100;
+            else
+                score -= 100;
+
+            var positionOnMarket = NicheUtils.GetPositionOnMarket(gameContext, startup) + 1;
+            score += 100 / positionOnMarket;
+
+            var quality = NicheUtils.GetAppQualityOnMarket(gameContext, startup) + 1;
+            score += 30 / quality;
+
+            score += startup.marketing.BrandPower;
+
+            return score;
+        }
+
         public static long GetDesireToBuyStartupAsGroup(GameContext gameContext, GameEntity group, GameEntity startup)
         {
             long score = 0;
@@ -18,18 +38,9 @@ namespace Assets.Utils
             if (IsInSphereOfInterest(group, startup))
                 score += 1000;
 
-            if (NicheUtils.IsPerspectiveNiche(gameContext, startup.product.Niche))
-                score += 100;
+            var attractiveness = GetStartupAttractiveness(gameContext, startup);
 
-            var positionOnMarket = NicheUtils.GetPositionOnMarket(gameContext, startup) + 1;
-
-            score += 100 / positionOnMarket;
-
-            var quality = NicheUtils.GetAppQualityOnMarket(gameContext, startup) + 1;
-
-            score += 30 / quality;
-
-
+            return score;
         }
     }
 }
---
+++ Assets/Utils/Companies/DesireToBuyOrSell/DesireToSell.cs
@@ -0,0 +1,14 @@
+﻿using Entitas;
+using System;
+using System.Linq;
+
+namespace Assets.Utils
+{
+    public static partial class CompanyUtils
+    {
+        public static long GetDesireToSell(GameEntity buyer, GameEntity target)
+        {
+            return 1;
+        }
+    }
+}
