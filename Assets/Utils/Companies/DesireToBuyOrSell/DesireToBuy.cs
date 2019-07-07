68e101c717688d466031db037ce64ed24eb8b4cf
gaga <23i03g@mail.ru>
2019-07-07 18:32:27 +0400

    Create DesireToBuy.cs

---
+++ Assets/Utils/Companies/DesireToBuyOrSell/DesireToBuy.cs
@@ -0,0 +1,33 @@
+﻿using Entitas;
+using System;
+using System.Linq;
+
+namespace Assets.Utils
+{
+    public static partial class CompanyUtils
+    {
+        public static long GetDesireToBuy(GameEntity buyer, GameEntity target)
+        {
+            return 1;
+        }
+
+        public static long GetDesireToBuyStartupAsGroup(GameEntity group, GameEntity startup)
+        {
+            long score = 0;
+
+            if (IsInSphereOfInterest(group, startup))
+                score += 1000;
+
+
+        }
+
+        public static bool IsPerspectiveNiche(GameContext gameContext, NicheType nicheType)
+        {
+            var niche = NicheUtils.GetNicheEntity(gameContext, nicheType);
+
+            var phase = niche.nicheState.Phase;
+
+            return phase == NicheLifecyclePhase.Innovation && phase == NicheLifecyclePhase.Trending && phase == NicheLifecyclePhase.MassUse;
+        }
+    }
+}
