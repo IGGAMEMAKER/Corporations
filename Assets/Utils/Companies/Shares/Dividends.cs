4f8d43c6459dcf3a5785daeb47ec77df9e86aa99
gaga <23i03g@mail.ru>
2019-07-08 08:52:41 +0400

    splitting

--- Assets/Utils/Companies/Shares/Dividends.cs
+++ Assets/Utils/Companies/Shares/Dividends.cs
@@ -6,6 +6,29 @@ namespace Assets.Utils
 {
     partial class CompanyUtils
     {
+        public static void PayDividends(GameContext gameContext, int companyId)
+        {
+            PayDividends(gameContext, GetCompanyById(gameContext, companyId));
+        }
 
+        public static void PayDividends(GameContext gameContext, GameEntity company)
+        {
+            int dividendSize = 33;
+            var balance = company.companyResource.Resources.money;
+
+            var dividends = balance * dividendSize / 100;
+
+            foreach (var s in company.shareholders.Shareholders)
+            {
+                var investorId = s.Key;
+                var sharePercentage = GetShareSize(gameContext, company.company.Id, investorId);
+
+                var sum = dividends * sharePercentage / 100;
+
+                AddMoneyToInvestor(gameContext, investorId, sum);
+            }
+
+            SpendResources(company, new Classes.TeamResource(dividends));
+        }
     }
 }
