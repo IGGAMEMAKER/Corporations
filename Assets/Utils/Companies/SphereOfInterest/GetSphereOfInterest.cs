11f24bd1cee8d0938929517aba97c8a87d29c139
gaga <23i03g@mail.ru>
2019-07-08 09:05:47 +0400

    consolidate sphere of influence file

--- Assets/Utils/Companies/SphereOfInterest/GetSphereOfInterest.cs
+++ Assets/Utils/Companies/SphereOfInterest/GetSphereOfInterest.cs
@@ -17,5 +17,48 @@ namespace Assets.Utils
 
             return IsInSphereOfInterest(company, interestingCompany.product.Niche);
         }
+
+
+
+
+        public static void RemoveFromSphereOfInfluence(NicheType nicheType, GameEntity company, GameContext gameContext)
+        {
+            var focus = company.companyFocus;
+            var niches = focus.Niches.Remove(nicheType);
+
+            NotifyAboutCompanyFocusChange(gameContext, company.company.Id, false, nicheType);
+
+            company.ReplaceCompanyFocus(focus.Niches, focus.Industries);
+        }
+
+        public static void AddFocusNiche(NicheType nicheType, GameEntity company, GameContext gameContext)
+        {
+            var niches = company.companyFocus.Niches;
+
+            if (niches.Contains(nicheType))
+                return;
+
+            niches.Add(nicheType);
+            //NotifyAboutCompanyFocusChange(gameContext, company.company.Id, true, nicheType);
+
+            company.ReplaceCompanyFocus(niches, company.companyFocus.Industries);
+        }
+
+        public static void NotifyAboutCompanyFocusChange(GameContext gameContext, int companyId, bool addedOrRemoved, NicheType nicheType)
+        {
+            NotificationUtils.AddNotification(gameContext, new NotificationMessageCompanyFocusChange(companyId, addedOrRemoved, nicheType));
+        }
+
+        public static void AddFocusIndustry(IndustryType industryType, GameEntity company)
+        {
+            var industries = company.companyFocus.Industries;
+
+            if (industries.Contains(industryType))
+                return;
+
+            industries.Add(industryType);
+
+            company.ReplaceCompanyFocus(company.companyFocus.Niches, industries);
+        }
     }
 }
