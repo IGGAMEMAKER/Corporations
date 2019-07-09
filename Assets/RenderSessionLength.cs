ebc4be9a9a7cffc3dcafe296d12ec5067998e2fb
gaga <23i03g@mail.ru>
2019-07-09 16:36:37 +0400

    show session length

---
+++ Assets/RenderSessionLength.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderSessionLength : MonoBehaviour
+{
+    //private void OnGUI()
+    //{
+    //    GUI.Label(new Rect(), "Session time: " + Time.time);
+    //}
+}
--- Assets/Singletons/Exit.cs
+++ Assets/Singletons/Exit.cs
@@ -16,9 +16,40 @@ public class Exit : View
 
         if (Input.GetKeyDown(KeyCode.F12))
         {
-            string time = System.DateTime.UtcNow.ToString("dd MMMM yyyy, HH-mm-ss");
-
-            ScreenCapture.CaptureScreenshot($"C://Users/Gaga/Desktop/Screenshots/save {time}.png");
+            MakeAScreenShot();
         }
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("SESSION TIME: " + (int)Time.time + " seconds");
+
+        PrintSessionLength();
+        PrintSessionLength();
+        PrintSessionLength();
+        PrintSessionLength();
+    }
+
+    public string FormatTime(float time)
+    {
+        int hours = (int)time / 3600;
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+
+        return string.Format("{3:00}h {0:00}min {1:00}sec", minutes, seconds, milliseconds, hours);
+        //return string.Format("{3:00}:{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds, hours);
+    }
+
+    void PrintSessionLength()
+    {
+        Debug.Log("SESSION TIME: " + FormatTime(Time.time));
+    }
+
+    void MakeAScreenShot()
+    {
+        string time = System.DateTime.UtcNow.ToString("dd MMMM yyyy, HH-mm-ss");
+
+        ScreenCapture.CaptureScreenshot($"C://Users/Gaga/Desktop/Screenshots/save {time}.png");
     }
 }
