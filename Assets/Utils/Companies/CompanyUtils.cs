80737aabc3341e3cace9638018d14782cff557c7
gaga <23i03g@mail.ru>
2019-07-12 21:39:59 +0400

    Update CompanyUtils.cs

--- Assets/Utils/Companies/CompanyUtils.cs
+++ Assets/Utils/Companies/CompanyUtils.cs
@@ -24,16 +24,26 @@ namespace Assets.Utils
 
         internal static GameEntity[] GetProductCompanies(GameContext gameContext)
         {
-            return gameContext.GetEntities(GameMatcher.Product);
+            return gameContext.GetEntities(GameMatcher.AllOf(GameMatcher.Product, GameMatcher.Alive));
         }
 
         internal static GameEntity[] GetAIProducts(GameContext gameContext)
         {
+            return GetProductCompanies(gameContext);
+
             return gameContext.GetEntities(
                     GameMatcher
                     .AllOf(GameMatcher.Product, GameMatcher.Alive)
                 //.NoneOf(GameMatcher.ControlledByPlayer)
                 );
+        }
+
+        internal static GameEntity[] GetGroupCompanies(GameContext gameContext)
+        {
+            return Array.FindAll(
+                gameContext.GetEntities(GameMatcher.AllOf(GameMatcher.Company, GameMatcher.Shareholder)),
+                e => IsManagingCompany(e)
+            );
         }
 
         internal static GameEntity[] GetAIManagingCompanies(GameContext gameContext)
@@ -71,13 +81,7 @@ namespace Assets.Utils
             );
         }
 
-        internal static GameEntity[] GetGroupCompanies(GameContext gameContext)
-        {
-            return Array.FindAll(
-                gameContext.GetEntities(GameMatcher.AllOf(GameMatcher.Company, GameMatcher.Shareholder)),
-                e => IsManagingCompany(e)
-            );
-        }
+
 
         public static bool IsManagingCompany(GameEntity e)
         {
@@ -86,13 +90,16 @@ namespace Assets.Utils
             return t == CompanyType.Corporation || t == CompanyType.Group || t == CompanyType.Holding;
         }
 
-
-
         public static bool IsCompanyGroupLike(GameContext context, int companyId)
         {
             var c = GetCompanyById(context, companyId);
 
             return IsCompanyGroupLike(c);
+        }
+
+        public static bool IsCompanyGroupLike(GameEntity gameEntity)
+        {
+            return !IsProductCompany(gameEntity);
         }
 
         public static bool IsProductCompany(GameEntity gameEntity)
@@ -105,10 +112,7 @@ namespace Assets.Utils
             return IsProductCompany(GetCompanyById(context, companyId));
         }
 
-        public static bool IsCompanyGroupLike(GameEntity gameEntity)
-        {
-            return !IsProductCompany(gameEntity);
-        }
+
 
         public static GameEntity[] GetInvestableCompanies(GameContext context)
         {
