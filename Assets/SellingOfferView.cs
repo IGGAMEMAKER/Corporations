7cbe87643c31cee65f2cb1466b53cb87c03c44ef
gaga <23i03g@mail.ru>
2019-07-16 20:54:27 +0400

    selling offer

--- Assets/SellingOfferView.cs
+++ Assets/SellingOfferView.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using Assets.Utils;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SellingOfferView : MonoBehaviour
+public class SellingOfferView : View
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text Name;
+    public Text Buyer;
+    public Text Valuation;
+    public Text Offer;
+
+    public Button AcceptOffer;
+    public Button RejectOffer;
+
+    int companyId, buyerId;
+
+    void SetEntity(int companyId, int buyerId)
     {
-        
+        this.companyId = companyId;
+        this.buyerId = buyerId;
+
+        Render();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Render()
     {
-        
+        var c = CompanyUtils.GetCompanyById(GameContext, companyId);
+        var buyer = CompanyUtils.GetInvestorById(GameContext, buyerId);
+
+        Name.text = c.company.Name;
+        Buyer.text = CompanyUtils.GetInvestorName(buyer);
+
+        Valuation.text = Format.Money(CompanyEconomyUtils.GetCompanyCost(GameContext, companyId));
+
+        var offer = CompanyUtils.GetAcquisitionOffer(GameContext, companyId, buyerId);
+        Offer.text = Format.Money(offer.acquisitionOffer.Offer);
+
+
     }
 }
