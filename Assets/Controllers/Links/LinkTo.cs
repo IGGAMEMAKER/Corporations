0ea0aab0879c6a827be4fad651ee60cf865a7f92
gaga <23i03g@mail.ru>
2019-07-16 17:28:46 +0400

    Update LinkTo.cs

--- Assets/Controllers/Links/LinkTo.cs
+++ Assets/Controllers/Links/LinkTo.cs
@@ -27,7 +27,8 @@ public class LinkTo : ButtonController
                 NavigateToCompany(TargetMenu, MyCompany.company.Id);
                 break;
             case ScreenMode.ProjectScreen:
-                NavigateToCompany(TargetMenu, SelectedCompany.company.Id);
+                //NavigateToCompany(TargetMenu, SelectedCompany.company.Id);
+                NavigateToCompany(TargetMenu, MyCompany.company.Id);
                 break;
             case ScreenMode.TeamScreen:
                 NavigateToCompany(TargetMenu, MyProductEntity.company.Id);
