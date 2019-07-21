f2cae5c72947f3c04dc2a0351cb27125d322905a
gaga <23i03g@mail.ru>
2019-07-21 17:49:16 +0400

    show sorted players on niche in niche screen

---
+++ Assets/PlayersOnNicheSorted.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayersOnNicheSorted : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        t.GetComponent<CompanyViewOnMap>().SetEntity(entity as GameEntity);
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var players = NicheUtils.GetPlayersOnMarket(GameContext, SelectedNiche).OrderByDescending(p => MarketingUtils.GetClients(p));
+
+        SetItems(players.ToArray());
+    }
+}
