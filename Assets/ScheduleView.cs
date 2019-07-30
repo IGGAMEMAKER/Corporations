f774aa4a0cb9f2a90cbc2dc224c9bf37a90b2a16
gaga <23i03g@mail.ru>
2019-07-30 13:45:23 +0400

    date string

--- Assets/ScheduleView.cs
+++ Assets/ScheduleView.cs
@@ -19,13 +19,35 @@ public class ScheduleView : View
 
         day = day % 30;
 
-        var DateT = new DateTime(year, 1, 1).AddMonths(month).AddDays(day);
 
+        var dateString = $"{day + 1:00} {GetMonthLiteral(month):000000000} {year:0000}";
         //var dateString = $"{day + 1:00} {month + 1:MM} {year:0000}";
-        var dateString = DateT.ToString("dd MMM yyyy");
+
+        //var DateT = new DateTime(year, 1, 1).AddMonths(month).AddDays(day);
+        //var dateString = DateT.ToString("dd MMM yyyy");
 
         ScheduleResourceView.UpdateResourceValue("Date", dateString);
 
         PauseStatus.gameObject.SetActive(!ScheduleUtils.IsTimerRunning(GameContext));
+    }
+
+    string GetMonthLiteral(int month)
+    {
+        switch (month)
+        {
+            case 0: return "January";
+            case 1: return "February";
+            case 2: return "March";
+            case 3: return "April";
+            case 4: return "May";
+            case 5: return "June";
+            case 6: return "July";
+            case 7: return "August";
+            case 8: return "September";
+            case 9: return "October";
+            case 10: return "November";
+            case 11: return "December";
+            default: return "UNKNOWN MONTH";
+        }
     }
 }
