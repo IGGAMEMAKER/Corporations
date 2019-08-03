f31cafc2816754627d468fb68e461dfb6dd2b9d8
gaga <23i03g@mail.ru>
2019-08-03 16:39:03 +0400

    Update NicheMapRenderer.cs

--- Assets/NicheMapRenderer.cs
+++ Assets/NicheMapRenderer.cs
@@ -66,7 +66,7 @@ public class NicheMapRenderer : View
     void RenderCompanies(NicheType niche, GameObject m)
     {
         var competitors = NicheUtils.GetPlayersOnMarket(GameContext, niche)
-            .OrderByDescending(p => CompanyUtils.GetCompanyExpertise(p) * 100 + ProductUtils.GetProductLevel(p)).ToArray();
+            .OrderByDescending(p => ProductUtils.GetProductLevel(p) * 1000 + CompanyUtils.GetCompanyExpertise(p)).ToArray();
 
         var marketPosition = m.transform.localPosition;
 
