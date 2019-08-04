4446ffaed1d16ee0fffa8d48958f13e7cc16a9b8
gaga <23i03g@mail.ru>
2019-08-04 22:02:03 +0400

    improving annual report screen

---
+++ Assets/CompanyGrowthPreview.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanyGrowthPreview : View
+{
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+
+    }
+
+    public void SetEntity(GameEntity c)
+    {
+
+    }
+}
---
+++ Assets/GroupStatsListView.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GroupStatsListView : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        t.GetComponent<CompanyGrowthPreview>().SetEntity(entity as GameEntity);
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var daughters = CompanyUtils.GetDaughterCompanies(GameContext, MyCompany.company.Id);
+
+        //daughters.OrderByDescending(d => )
+
+        SetItems(daughters);
+    }
+}
