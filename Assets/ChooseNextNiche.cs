62578aeaa7f5302d8d201210115ce4b0976d6f93
gaga <23i03g@mail.ru>
2019-08-04 16:38:18 +0400

    Update ChooseNextNiche.cs

--- Assets/ChooseNextNiche.cs
+++ Assets/ChooseNextNiche.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ChooseNextNiche : ButtonController
+﻿public class ChooseNextNiche : ButtonController
 {
     public override void Execute()
     {
