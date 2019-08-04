00e9f719020dab2ea74fb68a9e2646870dad84f5
gaga <23i03g@mail.ru>
2019-08-04 21:45:07 +0400

    annual report screen: added perspective niches and group stats mockups

---
+++ Assets/Lists/PerspectiveNichesListView.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Utils;
+using UnityEngine;
+
+public class PerspectiveNichesListView : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        t.GetComponent<NichePreview>().SetNiche(entity as GameEntity);
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var perspectiveNiches = NicheUtils.GetPlayableNiches(GameContext);
+
+        SetItems(perspectiveNiches);
+    }
+}
