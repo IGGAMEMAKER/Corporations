c4c3aef6f988b27a7a8ddd1eecbc66b11e6571c4
gaga <23i03g@mail.ru>
2019-08-08 17:01:03 +0400

    niche screen

--- Assets/RenderMarketState.cs
+++ Assets/RenderMarketState.cs
@@ -12,7 +12,19 @@ public class RenderMarketState : UpgradedParameterView
 
     public override string RenderValue()
     {
-        return State.ToString();
+        var str = "";
+
+        switch(State)
+        {
+            case NicheLifecyclePhase.Idle: str = "???"; break;
+            case NicheLifecyclePhase.Innovation: str = "Innovation"; break;
+            case NicheLifecyclePhase.Trending: str = "Trending"; break;
+            case NicheLifecyclePhase.MassUse: str = "Mass use"; break;
+            case NicheLifecyclePhase.Decay: str = "Decay"; break;
+            case NicheLifecyclePhase.Death: str = "Death"; break;
+        }
+
+        return str + " phase"; // State.ToString();
     }
 
     NicheLifecyclePhase State => NicheUtils.GetMarketState(GameContext, SelectedNiche);
