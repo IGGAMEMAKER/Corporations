34d59f70a091e4838c15892ad166fcc17ced1177
gaga <23i03g@mail.ru>
2019-09-01 16:45:43 +0400

    Update ProductPriceChangeView.cs

--- Assets/ProductPriceChangeView.cs
+++ Assets/ProductPriceChangeView.cs
@@ -1,11 +1,1 @@
-﻿public class ProductPriceChangeView : View
-{
-    public override void ViewRender()
-    {
-        base.ViewRender();
-
-        var c = GetComponent<ProductPriceIncreaseController>();
-
-        ToggleIsChosenComponent(MyProductEntity.finance.price == c.priceLevel);
-    }
-}
+﻿
