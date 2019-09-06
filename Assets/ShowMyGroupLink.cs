30091e464019627cd352ee9a0ceef01dd9569950
gaga <23i03g@mail.ru>
2019-09-06 20:47:55 +0400

    niche screen

--- Assets/ShowMyGroupLink.cs
+++ Assets/ShowMyGroupLink.cs
@@ -4,6 +4,7 @@ public class ShowMyGroupLink : HideOnSomeCondition
 {
     public override bool HideIf()
     {
+        return true;
         return CompanyUtils.GetDaughterCompanies(GameContext, MyCompany.company.Id).Length > 0;
     }
 }
