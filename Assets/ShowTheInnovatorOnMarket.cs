96449663da8b42e6a9f3f8d63226ba85806f7e6f
gaga <23i03g@mail.ru>
2019-09-11 20:28:13 +0400

    niche screen improvements

---
+++ Assets/ShowTheInnovatorOnMarket.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Utils;
+using System.Linq;
+
+public class ShowTheInnovatorOnMarket : UpgradedParameterView
+{
+    public override string RenderHint()
+    {
+        return "";
+    }
+
+    public override string RenderValue()
+    {
+        var players = NicheUtils.GetPlayersOnMarket(GameContext, SelectedNiche);
+
+        var productCompany = players.FirstOrDefault(p => p.isTechnologyLeader);
+
+        if (productCompany == null)
+            return "Noone has the decisive advantage in concept";
+
+        return $"{productCompany.company.Name} ({ProductUtils.GetProductLevel(productCompany)}LVL) \nThis gives them +1 Brand each month";
+    }
+}
---
+++ Assets/StrongestBrandOnMarketView.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StrongestBrandOnMarketView : UpgradedParameterView
+{
+    public override string RenderHint()
+    {
+        return "";
+    }
+
+    public override string RenderValue()
+    {
+        var players = NicheUtils.GetPlayersOnMarket(GameContext, SelectedNiche);
+
+        var productCompany = players.OrderByDescending(p => p.branding.BrandPower).First();
+
+        return $"{productCompany.company.Name} ({productCompany.branding.BrandPower})";
+    }
+}
