b7fcb3686d404085a7cd20504a4ebeca47e2206b
gaga <23i03g@mail.ru>
2019-09-15 22:20:15 +0400

    expertise

---
+++ Assets/RenderExpertise.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderExpertise : UpgradedParameterView
+{
+    public override string RenderHint()
+    {
+        return "";
+    }
+
+    public override string RenderValue()
+    {
+        return SelectedCompany.expertise.ExpertiseLevel.ToString();
+    }
+}
