3ff2817c1192f5b19cd4fcc70e278b2ddf7e1273
gaga <23i03g@mail.ru>
2019-09-16 18:44:56 +0400

    link to outdated products init

--- Assets/LinkToOutdatedProducts.cs
+++ Assets/LinkToOutdatedProducts.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Linq;
 
-public class LinkToUnhappyTeam : ButtonController
+public class LinkToOutdatedProducts : ButtonController
 {
     public override void Execute()
     {
