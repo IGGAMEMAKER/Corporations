a4673cc9ae2d12583afa4fe344bfb9be4e47f896
gaga <23i03g@mail.ru>
2019-09-16 18:23:04 +0400

    Update LinkToUnhappyTeam.cs

--- Assets/LinkToUnhappyTeam.cs
+++ Assets/LinkToUnhappyTeam.cs
@@ -32,6 +32,9 @@ public class LinkToUnhappyTeam : ButtonController
                 companyId = myUnhappyTeams[ind + 1].company.Id;
         }
 
+        var hint = $"You have {myUnhappyTeams.Length} exhausted teams.\n" + String.Join("\n", myUnhappyTeams.Select(p => p.company.Name));
+
+        GetComponent<Hint>().SetHint(hint);
         Navigate(targetMenu, Constants.MENU_SELECTED_COMPANY, companyId);
     }
 }
