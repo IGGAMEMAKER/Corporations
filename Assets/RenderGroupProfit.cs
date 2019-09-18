c30ecfb3db4114e3887c98835530ce755a32b2b1
gaga <23i03g@mail.ru>
2019-09-18 13:46:24 +0400

    new main screen

---
+++ Assets/RenderGroupProfit.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderGroupProfit : UpgradedParameterView
+{
+    public override string RenderHint()
+    {
+        return "";
+        var income = CompanyEconomyUtils.GetCompanyIncome(SelectedCompany, GameContext);
+        var maintenance = CompanyEconomyUtils.GetCompanyMaintenance(SelectedCompany, GameContext);
+
+        //CompanyEconomyUtils.incom
+
+        var hint = "Income: " + Visuals.Positive(Format.Money(income)) + "\n";
+            //"Team Maintenance: " + Visuals.Negative(Format.Money(maintenance)) +
+
+        return hint;
+    }
+
+    public override string RenderValue()
+    {
+        var change = CompanyEconomyUtils.GetBalanceChange(SelectedCompany, GameContext);
+
+        return Visuals.PositiveOrNegativeMinified(change);
+    }
+}
