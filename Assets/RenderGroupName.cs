92f493818fe80395b9cc7fcf4f23aa57823fe555
gaga <23i03g@mail.ru>
2019-09-26 20:58:42 +0400

    interface upgrade

---
+++ Assets/RenderGroupName.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UI;
+
+public class RenderGroupName : View
+{
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        GetComponent<Text>().text = MyCompany.company.Name;
+    }
+}
+
