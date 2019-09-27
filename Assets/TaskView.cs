9c4d46a2998cac956706b2c6b4aa57c4e2fac37a
gaga <23i03g@mail.ru>
2019-09-27 20:17:30 +0400

    Improving main screen

---
+++ Assets/TaskView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CompanyTaskType
+{
+    ExploreMarket,
+    ExploreCompany,
+
+    AcquiringCompany,
+
+}
+
+public class TaskView : View
+{
+    public Text Text;
+    int TaskId;
+
+    public void SetEntity(int taskId)
+    {
+        TaskId = taskId;
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        
+    }
+}
