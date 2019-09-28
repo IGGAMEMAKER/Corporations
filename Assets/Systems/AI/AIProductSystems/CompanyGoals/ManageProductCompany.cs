99aa8560cffbaf1e5f6048ed84576c72668d7fb3
gaga <23i03g@mail.ru>
2019-09-28 21:10:41 +0400

    Update ManageProductCompany.cs

--- Assets/Systems/AI/AIProductSystems/CompanyGoals/ManageProductCompany.cs
+++ Assets/Systems/AI/AIProductSystems/CompanyGoals/ManageProductCompany.cs
@@ -1,10 +1,42 @@
-﻿public partial class AIProductSystems : OnDateChange
+﻿using Assets.Utils;
+
+public partial class AIProductSystems : OnDateChange
 {
     void ManageProductDevelopment(GameEntity product)
     {
         UpgradeSegment(product);
+
+        ScaleTeamIfPossible(product);
+
+        PickTeamUpgrades(product);
     }
 
+
+
+    long GetProfit(GameEntity company)
+    {
+        return CompanyEconomyUtils.GetBalanceChange(company, gameContext);
+    }
+
+    void ManagePairOfWorkers(GameEntity product)
+    {
+
+    }
+
+    void PickTeamUpgrades(GameEntity product)
+    {
+
+    }
+
+    void ScaleTeamIfPossible(GameEntity product)
+    {
+        var cost = TeamUtils.GetTeamPromotionCost(product).money;
+
+        var profit = GetProfit(product);
+
+        if (profit > cost * 15 / 10)
+            TeamUtils.Promote(product);
+    }
 
     void ManageInvestors(GameEntity product)
     {
