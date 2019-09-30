d8e91e6c52643d67709df6df8ad17194aeefd723
gaga <23i03g@mail.ru>
2019-09-30 21:40:07 +0400

    Research company button

---
+++ Assets/ToggleCompanyActionButtonsIfCompanyIsNotResearched.cs
@@ -0,0 +1,10 @@
+﻿using Assets.Utils;
+
+public class ToggleCompanyActionButtonsIfCompanyIsNotResearched : ToggleOnSomeCondition
+{
+    public override bool Condition()
+    {
+        // SelectedCompany != null && (
+        return SelectedCompany.hasResearch || CompanyUtils.IsCompanyRelatedToPlayer(GameContext, SelectedCompany);
+    }
+}
