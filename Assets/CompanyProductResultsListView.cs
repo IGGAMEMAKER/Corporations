f7db4ec1a59b064d337799dd0acecb47c6c294e0
gaga <23i03g@mail.ru>
2019-10-03 16:08:03 +0400

    product companies report

---
+++ Assets/CompanyProductResultsListView.cs
@@ -0,0 +1,76 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public struct ProductCompanyResult
+{
+    public long clientChange;
+    public float MarketShareChange;
+    public ConceptStatus ConceptStatus;
+    public int CompanyId;
+}
+
+public class CompanyProductResultsListView : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        t.GetComponent<CompanyResultView>().SetEntity((ProductCompanyResult)(object)entity);
+    }
+
+    ProductCompanyResult GetProductCompanyResults (GameEntity product)
+    {
+        var competitors = NicheUtils.GetProductsOnMarket(GameContext, product);
+
+        long previousMarketSize = 0;
+        long currentMarketSize = 0;
+
+        long prevCompanyClients = 0;
+        long currCompanyClients = 0;
+
+        foreach (var c in competitors)
+        {
+            var last = c.metricsHistory.Metrics.Count - 1;
+            var prev = c.metricsHistory.Metrics.Count - 2;
+
+            // company was formed this month
+            if (prev < 0)
+                continue;
+
+            var audience = c.metricsHistory.Metrics[prev].AudienceSize;
+            var clients = c.metricsHistory.Metrics[last].AudienceSize;
+
+            previousMarketSize += audience;
+            currentMarketSize += clients;
+
+            if (c.company.Id == product.company.Id)
+            {
+                prevCompanyClients = audience;
+                currCompanyClients = clients;
+            }
+        }
+
+        var prevShare = prevCompanyClients * 100 / (previousMarketSize + 1);
+        var Share = currCompanyClients * 100 / (currentMarketSize + 1);
+
+        return new ProductCompanyResult {
+            clientChange = currCompanyClients - prevCompanyClients,
+            MarketShareChange = Share - prevShare,
+            ConceptStatus = ProductUtils.GetConceptStatus(product, GameContext),
+            CompanyId = product.company.Id
+        };
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var daughters = CompanyUtils.GetDaughterCompanies(GameContext, MyCompany.company.Id);
+        var results = daughters
+            .Select(GetProductCompanyResults)
+            .ToArray();
+
+        SetItems(results);
+    }
+}
---
+++ Assets/CompanyResultView.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompanyResultView : View
+{
+    public LinkToProjectView LinkToProjectView;
+    public Text CompanyName;
+    public Text ClientGrowth;
+    public Text MarketShareChange;
+    public Text ConceptStatusText;
+
+    public void SetEntity(ProductCompanyResult result)
+    {
+        CompanyName.text = CompanyUtils.GetCompanyById(GameContext, result.CompanyId).company.Name;
+
+        //ClientGrowth.color = Visuals.G
+        ClientGrowth.text = Format.Minify(result.clientChange);
+
+        MarketShareChange.text = result.MarketShareChange.ToString();
+
+        ConceptStatusText.text = result.ConceptStatus.ToString();
+
+        LinkToProjectView.CompanyId = result.CompanyId;
+    }
+}
