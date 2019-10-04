dbb1dc178a1f21de8381da059d938ee8398f83f1
gaga <23i03g@mail.ru>
2019-10-05 00:48:21 +0400

    acquisition screen almost done

--- Assets/AcquisitionScreen.cs
+++ Assets/AcquisitionScreen.cs
@@ -17,6 +17,11 @@ public class AcquisitionScreen : View
 
     public Toggle KeepFounderAsCEO;
 
+    public Text SharePercentage;
+
+    public InputField CashOfferInput;
+    public InputField SharesOfferInput;
+
     public override void ViewRender()
     {
         base.ViewRender();
@@ -50,7 +55,10 @@ public class AcquisitionScreen : View
 
         Offer.text = Format.Money(offer) + overpriceText;
         SellerPrice.text = Format.Money(conditions.SellerPrice);
+        
 
+        CashOfferInput.text = offer.ToString();
+        SharesOfferInput.text = conditions.ByShares.ToString();
 
         TriesRemaining.text = acquisitionOffer.RemainingTries.ToString();
         DaysRemaining.text = acquisitionOffer.RemainingDays + " days left";
@@ -59,12 +67,73 @@ public class AcquisitionScreen : View
         AcquisitionButtonView.SetAcquisitionBid(offer, isWillSell);
 
         KeepFounderAsCEO.isOn = conditions.KeepLeaderAsCEO;
+
+        var ourCompanyCost = EconomyUtils.GetCompanyCost(GameContext, MyCompany);
+        var sharePercent = conditions.ByShares;
+        var shareCost = sharePercent * ourCompanyCost / 100;
+
+        SharePercentage.text = sharePercent + "% (worth " + Format.Money(shareCost) + ")";
     }
 
     public void ToggleKeepFounderAsCEO()
     {
         AcquisitionOffer.AcquisitionConditions.KeepLeaderAsCEO = KeepFounderAsCEO.isOn;
     }
+
+    void UpdateData()
+    {
+        CompanyUtils.TweakAcquisitionConditions(GameContext, SelectedCompany.company.Id, MyCompany.shareholder.Id, Conditions);
+
+        ScreenUtils.UpdateScreenWithoutAnyChanges(GameContext);
+    }
+
+    // TODO move to ButtonController classes
+    public void IncreaseShareOffer()
+    {
+        var newConditions = Conditions;
+
+        newConditions.ByShares = Mathf.Clamp(newConditions.ByShares + 1, 0, 100);
+
+        //CompanyUtils.TweakAcquisitionConditions(GameContext, SelectedCompany.company.Id, MyCompany.shareholder.Id, newConditions);
+
+        //ScreenUtils.UpdateScreenWithoutAnyChanges(GameContext);
+    }
+    public void DecreaseShareOffer()
+    {
+        var newConditions = Conditions;
+
+        newConditions.ByShares = Mathf.Clamp(newConditions.ByShares - 1, 0, 100);
+
+        CompanyUtils.TweakAcquisitionConditions(GameContext, SelectedCompany.company.Id, MyCompany.shareholder.Id, newConditions);
+
+        ScreenUtils.UpdateScreenWithoutAnyChanges(GameContext);
+    }
+
+    public void OnCashOfferEdit()
+    {
+        var offer = long.Parse(CashOfferInput.text);
+
+        //var newConditions = Conditions;
+        Conditions.BuyerOffer = offer;
+
+        UpdateData();
+
+        //CompanyUtils.TweakAcquisitionConditions(GameContext, SelectedCompany.company.Id, MyCompany.shareholder.Id, newConditions);
+        //ScreenUtils.UpdateScreenWithoutAnyChanges(GameContext);
+    }
+
+    public void OnSharesOfferEdit()
+    {
+        var offer = int.Parse(SharesOfferInput.text);
+
+        Conditions.ByShares = Mathf.Clamp(offer, 0, 100);
+
+        UpdateData();
+        //CompanyUtils.TweakAcquisitionConditions(GameContext, SelectedCompany.company.Id, MyCompany.shareholder.Id, newConditions);
+        //ScreenUtils.UpdateScreenWithoutAnyChanges(GameContext);
+    }
+
+    AcquisitionConditions Conditions => AcquisitionOffer.AcquisitionConditions;
 
     AcquisitionOfferComponent AcquisitionOffer
     {
