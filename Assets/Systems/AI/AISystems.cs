644061e4d4c4aac7ec635bb7826c892dee7d3d36
gaga <23i03g@mail.ru>
2019-10-06 23:11:23 +0400

    refactoring

--- Assets/Systems/AI/AISystems.cs
+++ Assets/Systems/AI/AISystems.cs
@@ -2,14 +2,16 @@
 {
     public AISystems(Contexts contexts) : base("AI Systems")
     {
+        // products
         Add(new AIProductSystems(contexts));
-
         Add(new ProductCompaniesPayDividendsSystem(contexts));
-
-        Add(new AIInvestmentSystems(contexts));
-
         Add(new AIPromoteProductToGroup(contexts));
 
+        // both groups and products
+        Add(new AIInvestmentSystems(contexts));
+
+
+        // groups
         Add(new AIGroupSystems(contexts));
     }
 }
