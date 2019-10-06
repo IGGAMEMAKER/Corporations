2962e34e2165289e7e870312c54eaef2817b2f49
gaga <23i03g@mail.ru>
2019-10-07 01:05:18 +0400

    Update NicheCosts.cs

--- Assets/Utils/Niches/NicheCosts.cs
+++ Assets/Utils/Niches/NicheCosts.cs
@@ -97,52 +97,5 @@ namespace Assets.Utils
 
             return new TeamResource(0, 0, baseCosts.salesPoints * 3, 0, baseCosts.money * 20);
         }
-
-        //#region branding
-        //public static TeamResource GetPureBrandingCost(GameEntity niche)
-        //{
-        //    var costs = NicheUtils.GetNicheCosts(niche);
-
-        //    return new TeamResource(0, 0, costs.MarketingCost * 4, 0, costs.AdCost * 3);
-        //}
-
-        //public static TeamResource GetPureBrandingCost(GameContext gameContext, NicheType nicheType)
-        //{
-        //    var niche = NicheUtils.GetNicheEntity(gameContext, nicheType);
-
-        //    return GetPureBrandingCost(niche);
-        //}
-
-        //public static TeamResource GetPureBrandingCost(GameContext gameContext, GameEntity company)
-        //{
-        //    return GetPureBrandingCost(gameContext, company.product.Niche);
-        //}
-
-        ////public static TeamResource GetPureBrandingCostPerMonth(GameEntity niche)
-        ////{
-
-        ////}
-        //#endregion
-
-        //#region targeting
-        //public static TeamResource GetPureTargetingCost(GameEntity niche)
-        //{
-        //    var costs = NicheUtils.GetNicheCosts(niche);
-
-        //    return new TeamResource(0, 0, costs.MarketingCost * 1, 0, costs.AdCost * 1);
-        //}
-
-        //public static TeamResource GetPureTargetingCost(GameContext gameContext, NicheType nicheType)
-        //{
-        //    var niche = NicheUtils.GetNicheEntity(gameContext, nicheType);
-
-        //    return GetPureTargetingCost(niche);
-        //}
-
-        //public static TeamResource GetPureTargetingCost(GameContext gameContext, GameEntity company)
-        //{
-        //    return GetPureTargetingCost(gameContext, company.product.Niche);
-        //}
-        //#endregion
     }
 }
