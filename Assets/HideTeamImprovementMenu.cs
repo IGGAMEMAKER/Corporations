4f3287ab2592667c93b72b7fec30b0837be146b3
gaga <23i03g@mail.ru>
2019-10-06 23:43:56 +0400

    remove team improvement screen

---
+++ Assets/HideTeamImprovementMenu.cs
@@ -0,0 +1,9 @@
+﻿using Assets.Utils;
+
+public class HideTeamImprovementMenu : HideOnSomeCondition
+{
+    public override bool HideIf()
+    {
+        return !(SelectedCompany.hasProduct && CompanyUtils.IsDaughterOfCompany(MyCompany, SelectedCompany));
+    }
+}
