9f4e683d73cb107954e0f1577da16ca7c7fe5dc8
gaga <23i03g@mail.ru>
2019-10-09 13:51:30 +0400

    Company cost to separate file

---
+++ Assets/Utils/Economy/CompanyCost.cs
@@ -0,0 +1,57 @@
+﻿namespace Assets.Utils
+{
+    public static partial class EconomyUtils
+    {
+        public static long GetCompanyCost(GameContext context, GameEntity c)
+        {
+            long cost;
+            if (CompanyUtils.IsProductCompany(c))
+                cost = GetProductCompanyCost(context, c.company.Id);
+            else
+                cost = GetGroupOfCompaniesCost(context, c);
+
+            long capital = c.companyResource.Resources.money;
+
+            // +1 to avoid division by zero
+            return cost + capital + 1;
+        }
+
+        public static long GetCompanyCost(GameContext context, int companyId)
+        {
+            var c = CompanyUtils.GetCompanyById(context, companyId);
+
+            return GetCompanyCost(context, c);
+        }
+
+        public static long GetCompanySellingPrice(GameContext context, int companyId)
+        {
+            var target = CompanyUtils.GetCompanyById(context, companyId);
+
+            var desireToSell = CompanyUtils.GetDesireToSellCompany(target, context);
+
+            return GetCompanyCost(context, companyId) * desireToSell;
+        }
+
+        public static long GetCompanyCostNicheMultiplier()
+        {
+            return 15;
+        }
+
+        public static long GetCompanyBaseCost(GameContext context, int companyId)
+        {
+            var c = CompanyUtils.GetCompanyById(context, companyId);
+
+            if (CompanyUtils.IsProductCompany(c))
+                return GetProductCompanyBaseCost(context, companyId);
+
+            return GetCompanyCost(context, companyId);
+        }
+
+        public static long GetCompanyIncomeBasedCost(GameContext context, int companyId)
+        {
+            var c = CompanyUtils.GetCompanyById(context, companyId);
+
+            return GetCompanyIncome(c, context) * GetCompanyCostNicheMultiplier();
+        }
+    }
+}
--- Assets/Utils/Economy/CompanyEconomyUtils.cs
+++ Assets/Utils/Economy/CompanyEconomyUtils.cs
@@ -2,14 +2,6 @@
 {
     public static partial class EconomyUtils
     {
-        public static long GetCompanyIncome(GameEntity e, GameContext context)
-        {
-            if (CompanyUtils.IsProductCompany(e))
-                return GetProductCompanyIncome(e, context);
-
-            return GetGroupIncome(context, e);
-        }
-
         public static long GetCompanyIncome(int companyId, GameContext context)
         {
             var e = CompanyUtils.GetCompanyById(context, companyId);
@@ -17,59 +9,13 @@
             return GetCompanyIncome(e, context);
         }
 
-        public static long GetCompanyCost(GameContext context, GameEntity c)
+        public static long GetCompanyIncome(GameEntity e, GameContext context)
         {
-            long cost;
-            if (CompanyUtils.IsProductCompany(c))
-                cost = GetProductCompanyCost(context, c.company.Id);
-            else
-                cost = GetGroupOfCompaniesCost(context, c);
+            if (CompanyUtils.IsProductCompany(e))
+                return GetProductCompanyIncome(e, context);
 
-            long capital = c.companyResource.Resources.money;
-
-            // +1 to avoid division by zero
-            return cost + capital + 1;
+            return GetGroupIncome(context, e);
         }
-
-        public static long GetCompanyCost(GameContext context, int companyId)
-        {
-            var c = CompanyUtils.GetCompanyById(context, companyId);
-
-            return GetCompanyCost(context, c);
-        }
-
-        public static long GetCompanySellingPrice(GameContext context, int companyId)
-        {
-            var target = CompanyUtils.GetCompanyById(context, companyId);
-
-            var desireToSell = CompanyUtils.GetDesireToSellCompany(target, context);
-
-            return GetCompanyCost(context, companyId) * desireToSell;
-        }
-
-        public static long GetCompanyCostNicheMultiplier()
-        {
-            return 15;
-        }
-
-        public static long GetCompanyBaseCost(GameContext context, int companyId)
-        {
-            var c = CompanyUtils.GetCompanyById(context, companyId);
-
-            if (CompanyUtils.IsProductCompany(c))
-                return GetProductCompanyBaseCost(context, companyId);
-
-            return GetCompanyCost(context, companyId);
-        }
-
-        public static long GetCompanyIncomeBasedCost(GameContext context, int companyId)
-        {
-            var c = CompanyUtils.GetCompanyById(context, companyId);
-
-            return GetCompanyIncome(c, context) * GetCompanyCostNicheMultiplier();
-        }
-
-
 
         internal static long GetBalanceChange(GameEntity c, GameContext context)
         {
@@ -82,6 +28,7 @@
 
             return GetBalanceChange(c, context);
         }
+
 
         internal static bool IsProfitable(GameContext gameContext, int companyId)
         {
