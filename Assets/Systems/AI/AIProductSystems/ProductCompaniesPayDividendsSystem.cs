33b949bf20371517d7442c38be19cadc576d59c7
gaga <23i03g@mail.ru>
2019-10-10 19:33:45 +0400

    Update ProductCompaniesPayDividendsSystem.cs

--- Assets/Systems/AI/AIProductSystems/ProductCompaniesPayDividendsSystem.cs
+++ Assets/Systems/AI/AIProductSystems/ProductCompaniesPayDividendsSystem.cs
@@ -1,7 +1,7 @@
 ﻿using Assets.Utils;
 using System.Collections.Generic;
 
-public partial class ProductCompaniesPayDividendsSystem : OnMonthChange
+public partial class ProductCompaniesPayDividendsSystem : OnPeriodChange
 {
     public ProductCompaniesPayDividendsSystem(Contexts contexts) : base(contexts) {
     }
@@ -17,21 +17,21 @@ public partial class ProductCompaniesPayDividendsSystem : OnMonthChange
         if (product.isIndependentCompany)
             return;
 
-        var balanceChange = EconomyUtils.GetProfit(product, gameContext);
 
         if (CompanyUtils.IsCompanyRelatedToPlayer(gameContext, product))
         {
-            CompanyUtils.PayDividends(gameContext, product, balanceChange);
+            var profit = EconomyUtils.GetProfit(product, gameContext);
+            CompanyUtils.PayDividends(gameContext, product, profit);
+
             return;
         }
-
 
         if (EconomyUtils.IsCompanyNeedsMoreMoneyOnMarket(gameContext, product))
             return;
 
-        //if (balanceChange)
-        long dividends = product.companyResource.Resources.money;
 
+
+        long dividends = product.companyResource.Resources.money;
         CompanyUtils.PayDividends(gameContext, product, dividends);
     }
 }
