771c6bc36f305ddcec24b4a596b5d1583218f108
gaga <23i03g@mail.ru>
2019-11-13 20:27:08 +0400

    restructuring code

--- Assets/Utils/Marketing/AudienceGrowth.cs
+++ Assets/Utils/Marketing/AudienceGrowth.cs
@@ -29,18 +29,6 @@ namespace Assets.Utils
                 ;
         }
 
-        public static long GetAudienceGrowth(GameEntity product, GameContext gameContext)
-        {
-            var baseGrowth = GetCurrentClientFlow(gameContext, product.product.Niche) / 100;
-            var clients = GetClients(product);
-            var multiplier = GetAudienceGrowthMultiplier(product, gameContext);
-
-            if (multiplier < 0)
-                multiplier = 0;
-
-            return baseGrowth + (long)(multiplier * clients) / 100;
-        }
-
 
 
         // based on market state
