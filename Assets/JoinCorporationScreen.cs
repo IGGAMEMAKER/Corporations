0fdfc4e66fa5adec41e90a46498b3285a855e096
gaga <23i03g@mail.ru>
2019-11-15 18:31:21 +0400

    join corporation screen view

--- Assets/JoinCorporationScreen.cs
+++ Assets/JoinCorporationScreen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Assets.Utils;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,14 +9,12 @@ public class JoinCorporationScreen : View
     public Text Title;
     public Text ProposalStatus;
 
-    public Text Offer;
-    public Text SellerPrice;
+    public Text OurValuation;
+    public Text TargetValuation;
 
-    public Toggle KeepFounderAsCEO;
+    public Toggle KeepAsIndependent;
 
     public Text SharePercentage;
-
-    public InputField CashOfferInput;
     public InputField SharesOfferInput;
 
     public override void ViewRender()
@@ -26,5 +25,15 @@ public class JoinCorporationScreen : View
             return;
 
         Title.text = $"Integrate {SelectedCompany.company.Name} company to our corporation";
+
+        // TODO DIVIDE BY ZERO
+        var ourCost = EconomyUtils.GetCompanyCost(GameContext, MyCompany);
+        if (ourCost == 0) ourCost = 1;
+        var targetCost = EconomyUtils.GetCompanyCost(GameContext, SelectedCompany);
+
+        var sizeComparison = targetCost * 100 / ourCost;
+
+        OurValuation.text = Format.Money(ourCost);
+        TargetValuation.text = $"{Format.Money(targetCost)} ({sizeComparison}%)";
     }
 }
