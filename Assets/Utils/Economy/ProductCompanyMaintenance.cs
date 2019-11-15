49129e2c8f3192a9918b9f0d71758402db4a1ebc
gaga <23i03g@mail.ru>
2019-11-15 16:29:24 +0400

    Moved Product Company Maintenance to separate file

---
+++ Assets/Utils/Economy/ProductCompanyMaintenance.cs
@@ -0,0 +1,34 @@
+﻿namespace Assets.Utils
+{
+    partial class EconomyUtils
+    {
+        // resulting costs
+        internal static long GetProductCompanyMaintenance(GameEntity e, GameContext gameContext)
+        {
+            var devFinancing = GetProductDevelopmentCost(e, gameContext);
+            var marketingFinancing = GetProductMarketingCost(e, gameContext);
+
+            return devFinancing + marketingFinancing;
+        }
+
+        public static long GetProductMarketingCost(GameEntity e, GameContext gameContext)
+        {
+            var multiplier = GetMarketingFinancingCostMultiplier(e);
+            var gainedClients = MarketingUtils.GetAudienceGrowth(e, gameContext);
+
+            var acquisitionCost = NicheUtils.GetClientAcquisitionCost(e.product.Niche, gameContext);
+
+            return gainedClients * acquisitionCost * multiplier;
+        }
+
+        public static long GetProductDevelopmentCost(GameEntity e, GameContext gameContext)
+        {
+            var stage = GetStageFinancingMultiplier(e);
+            var team = GetTeamFinancingMultiplier(e);
+
+            var baseCost = NicheUtils.GetBaseDevelopmentCost(e.product.Niche, gameContext);
+
+            return (long)(baseCost * stage * team);
+        }
+    }
+}
