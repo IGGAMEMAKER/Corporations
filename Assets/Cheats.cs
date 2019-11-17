3fa27f5af44860121a3a84a3b78ad678698f361b
gaga <23i03g@mail.ru>
2019-11-18 02:05:56 +0400

    money cheat

---
+++ Assets/Cheats.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheats : View
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F8))
+        {
+            EconomyUtils.IncreaseCompanyBalance(GameContext, MyCompany.company.Id, 1000000000);
+            RefreshPage();
+        }
+    }
+}
