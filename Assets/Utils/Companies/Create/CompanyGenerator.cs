2006a944acc4aa2d70a4f63eef2c5c9e50254826
gaga <23i03g@mail.ru>
2019-11-22 17:33:45 +0400

    Update CompanyGenerator.cs

--- Assets/Utils/Companies/Create/CompanyGenerator.cs
+++ Assets/Utils/Companies/Create/CompanyGenerator.cs
@@ -8,35 +8,29 @@ namespace Assets.Utils
     {
         public static GameEntity CreateProduct(GameContext context, GameEntity company, NicheType niche)
         {
-            var GOAL = InvestorGoal.Operationing;
+            // positioning
+            company.AddProduct(niche, 0);
+            int positionings = NicheUtils.GetNichePositionings(niche, context).Count;
+            company.AddProductPositioning(Random.Range(0, positionings));
 
-            // product specific components
-            company.AddProduct(niche, 0); //  UnityEngine.Random.Range(0, 6)
-
+            // development
             company.AddProductImprovements(new Dictionary<ProductImprovement, int> { [ProductImprovement.Acquisition] = 0, [ProductImprovement.Monetisation] = 0, [ProductImprovement.Retention] = 0 }, 0);
-            company.AddExpertise(Random.Range(1, 4)); //  UnityEngine.Random.Range(0, 6)
+            company.AddExpertise(Random.Range(1, 4));
+            company.AddFinancing(new Dictionary<Financing, int> { [Financing.Development] = 0, [Financing.Marketing] = 0, [Financing.Team] = 0 });
 
+            // clients
             var flow = MarketingUtils.GetClientFlow(context, niche);
             var baseClients = Random.Range(flow / 2, flow);
             company.AddMarketing((long)baseClients);
-            company.AddFinancing(new Dictionary<Financing, int>
-            {
-                [Financing.Development] = 0,
-                [Financing.Marketing] = 0,
-                [Financing.Team] = 0
-            });
+
+            // sphere of interest
+            var industry = NicheUtils.GetIndustry(niche, context);
 
             AddFocusNiche(niche, company, context);
-
-            var industry = NicheUtils.GetIndustry(niche, context);
             AddFocusIndustry(industry, company);
 
 
-            InvestmentUtils.SetCompanyGoal(context, company, GOAL);
-
-            int positionings = NicheUtils.GetNichePositionings(niche, context).Count;
-
-            company.AddProductPositioning(Random.Range(0, positionings));
+            InvestmentUtils.SetCompanyGoal(context, company, InvestorGoal.Operationing);
 
             return company;
         }
@@ -53,23 +47,16 @@ namespace Assets.Utils
         {
             var e = context.CreateEntity();
 
-            e.isAlive = true;
-            e.isIndependentCompany = true;
-
-            var culture = new Dictionary<CorporatePolicy, int>()
-            {
-                [CorporatePolicy.Responsibility] = Random.Range(1, 6),
-                [CorporatePolicy.WorkerMindset] = Random.Range(1, 6),
-                [CorporatePolicy.Focusing] = Random.Range(1, 6),
-                [CorporatePolicy.CreateOrBuy] = Random.Range(1, 6),
-            };
-            e.AddCorporateCulture(culture);
-
             int id = GenerateCompanyId(context);
 
             e.AddCompany(id, name, companyType);
+            e.isAlive = true;
+            e.isIndependentCompany = true;
+
+
             e.AddCompanyResource(new TeamResource(100, 100, 100, 100, 10000));
 
+            // investments
             e.AddShareholders(founders);
             e.AddInvestmentProposals(new List<InvestmentProposal>());
             e.AddInvestmentRounds(InvestmentRound.Preseed);
@@ -78,12 +65,23 @@ namespace Assets.Utils
 
             e.AddBranding(0);
 
+            // team
             int CeoID = CEO.human.Id;
             e.AddCEO(0, CeoID);
             e.AddTeam(100, new Dictionary<int, WorkerRole>(), TeamStatus.Solo);
             TeamUtils.AttachToTeam(e, CeoID, WorkerRole.Universal);
             HumanUtils.SetSkills(CEO, WorkerRole.Business);
             HumanUtils.AttachToCompany(CEO, id, WorkerRole.Universal);
+
+            // culture
+            var culture = new Dictionary<CorporatePolicy, int>()
+            {
+                [CorporatePolicy.Responsibility] = Random.Range(1, 6),
+                [CorporatePolicy.WorkerMindset] = Random.Range(1, 6),
+                [CorporatePolicy.Focusing] = Random.Range(1, 6),
+                [CorporatePolicy.CreateOrBuy] = Random.Range(1, 6),
+            };
+            e.AddCorporateCulture(culture);
 
 
             e.AddCompanyFocus(new List<NicheType>(), new List<IndustryType>());
