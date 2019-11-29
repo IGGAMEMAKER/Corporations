f735729291fee6f436fbd32ac19c558658bea55a
gaga <23i03g@mail.ru>
2019-11-29 21:08:47 +0400

    render company cost on main screen

---
+++ Assets/RenderCompanyRank.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCompanyRank : UpgradedParameterView
+{
+    public override string RenderHint()
+    {
+        return "";
+    }
+
+    public override string RenderValue()
+    {
+        var rank = EconomyUtils.GetCompanyCost(GameContext, MyCompany);
+
+        return $"Company cost: {Format.Money(rank)}";
+    }
+}
