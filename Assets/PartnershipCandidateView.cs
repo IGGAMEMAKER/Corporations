0a877029482df3872b8167163a0745e89c4929c5
gaga <23i03g@mail.ru>
2019-12-06 03:32:18 +0400

    partnership candidates

---
+++ Assets/PartnershipCandidateView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartnershipCandidateView : View
+{
+    GameEntity GameEntity;
+
+    public Text CompanyName;
+    public Text BrandPowerGain;
+    public Text Opinion;
+    public Text TargetIndustry;
+
+    public void SetEntity(GameEntity gameEntity)
+    {
+        GameEntity = gameEntity;
+
+        CompanyName.text = gameEntity.company.Name;
+    }
+}
---
+++ Assets/PartnershipCandidates.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PartnershipCandidates : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        t.GetComponent<PartnershipCandidateView>().SetEntity(entity as GameEntity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var possiblePartners = CompanyUtils.GetIndependentCompanies(GameContext)
+            .Where(c => CompanyUtils.IsHaveIntersectingMarkets(MyCompany, c, GameContext));
+
+        SetItems(possiblePartners.ToArray());
+    }
+}
