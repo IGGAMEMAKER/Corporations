8115a1f6369783ec42dd34321484fdd47d026bcc
gaga <23i03g@mail.ru>
2019-12-06 20:06:32 +0400

    Update Partnerships.cs

--- Assets/Utils/Companies/Relationships/Partnerships.cs
+++ Assets/Utils/Companies/Relationships/Partnerships.cs
@@ -13,6 +13,7 @@ namespace Assets.Utils
                 .AppendAndHideIfZero("Have common markets", IsHaveIntersectingMarkets(requester, acceptor, gameContext) ? -90 : 0)
                 .AppendAndHideIfZero("Max amount of partners", IsHasTooManyPartnerships(acceptor) ? -75 : 0)
                 .Append("Partnership benefits", (long)GetCompanyBenefitFromTargetCompany(acceptor, requester, gameContext))
+                //.Append("Partnership benefits", (long)GetCompanyBenefitFromTargetCompany(requester, acceptor, gameContext))
                 ;
         }
 
