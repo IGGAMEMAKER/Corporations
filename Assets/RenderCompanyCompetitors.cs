64c34a2d8edde027b5503ad1840715547efcd0c5
gaga <23i03g@mail.ru>
2019-12-17 21:12:18 +0400

    Partners and Competitors

---
+++ Assets/RenderCompanyCompetitors.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCompanyCompetitors : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        var companyId = (int)(object)entity;
+
+        t.GetComponent<LinkToProjectView>().CompanyId = companyId;
+        t.GetComponent<RenderPartnerName>().SetCompanyId(companyId);
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var competitors = SelectedCompany.partnerships.companies;
+
+        SetItems(competitors.ToArray());
+    }
+}
