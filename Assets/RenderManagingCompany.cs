93e96ce1f993d9774b658365ec7566e41d064a9c
gaga <23i03g@mail.ru>
2019-12-19 19:34:11 +0400

    render managing company

---
+++ Assets/RenderManagingCompany.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Utils;
+using UnityEngine.UI;
+
+public class RenderManagingCompany : View
+{
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var managingCompany = Companies.GetManagingCompanyOf(SelectedCompany, GameContext);
+
+        bool isIndependent = managingCompany.company.Id == SelectedCompany.company.Id;
+
+        GetComponent<Text>().text = isIndependent ? "Independent company" : "Daughter of " + Visuals.Link(managingCompany.company.Name);
+
+        GetComponent<LinkToProjectView>().CompanyId = managingCompany.company.Id;
+        GetComponent<LinkToProjectView>().enabled = !isIndependent;
+    }
+}
