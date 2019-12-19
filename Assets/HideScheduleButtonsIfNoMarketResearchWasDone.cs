e4dcd290156de2465a8fe2eaec326ed8a0f00fe4
gaga <23i03g@mail.ru>
2019-12-19 17:17:14 +0400

    Better tutorial

---
+++ Assets/HideScheduleButtonsIfNoMarketResearchWasDone.cs
@@ -0,0 +1,9 @@
+﻿using Assets.Utils;
+
+public class HideScheduleButtonsIfNoMarketResearchWasDone : HideOnSomeCondition
+{
+    public override bool HideIf()
+    {
+        return CurrentIntDate == 0 && !CooldownUtils.IsHasTask(GameContext, new CompanyTaskExploreMarket(SelectedNiche));
+    }
+}
