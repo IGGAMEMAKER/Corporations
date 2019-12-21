9cf75b8390b6b98f99a86363a32efe9f5597ab65
gaga <23i03g@mail.ru>
2019-12-21 17:40:33 +0400

    Update CompanyManagementSystems.cs

--- Assets/Systems/AI/CompanyManagementSystems.cs
+++ Assets/Systems/AI/CompanyManagementSystems.cs
@@ -4,20 +4,23 @@
     {
         Add(new HistoricalProductSpawnSystem(contexts));
 
+        // products
         Add(new ProductResourceSystems(contexts));
         Add(new AutoUpgradeProducts(contexts));
         Add(new ManageProductMarketingSystem(contexts));
 
+        // dividends
         Add(new ProductCompaniesPayDividendsSystem(contexts));
 
         // investments
         Add(new AIInvestmentSystem(contexts));
         Add(new AISupportProductsSystem(contexts));
 
-
+        // expansion
         Add(new AIPromoteProductToGroup(contexts));
         Add(new AIGroupExpansionSystem(contexts));
 
+        // acquisitions
         Add(new ProcessAcquisitionOffersSystem(contexts));
 
         // bankruptcies
