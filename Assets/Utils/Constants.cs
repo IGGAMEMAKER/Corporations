51c8f7550450bca91137f958eccb668550f84269
gaga <23i03g@mail.ru>
2019-12-22 17:21:25 +0400

    Update Constants.cs

--- Assets/Utils/Constants.cs
+++ Assets/Utils/Constants.cs
@@ -52,6 +52,7 @@
 
     public static int COOLDOWN_CONCEPT = 15;
     internal static int RELEASE_BRAND_POWER_GAIN = 15;
+    internal static int INNOVATION_BRAND_POWER_GAIN = 5;
     internal const int CORPORATE_CULTURE_CHANGES_DURATION = 180;
 
     //public static int START_YEAR = 1991;
