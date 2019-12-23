509eca64cfe0e71b404ae76af79c1135b9ff7d13
gaga <23i03g@mail.ru>
2019-12-23 12:49:38 +0400

    Update DesireToSell.cs

--- Assets/Utils/Companies/DesireToBuyOrSell/DesireToSell.cs
+++ Assets/Utils/Companies/DesireToBuyOrSell/DesireToSell.cs
@@ -5,9 +5,9 @@
         public static bool IsWillSellCompany(GameEntity target, GameContext gameContext)
         {
             var desire = GetDesireToSellCompany(target, gameContext);
-            var founderWantsToSell = GetFounderAmbition(target, gameContext) == Ambition.EarnMoney;
+            //var founderWantsToSell = GetFounderAmbition(target, gameContext) == Ambition.EarnMoney;
 
-            return desire > 75 || founderWantsToSell || target.isOnSales || IsCompanyRelatedToPlayer(gameContext, target);
+            return desire > 75 || target.isOnSales || IsCompanyRelatedToPlayer(gameContext, target);
         }
 
         public static long GetDesireToSellCompany(GameEntity company, GameContext gameContext)
