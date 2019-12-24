a8779b94f94b91394e84ce86bec37f0752f5da3c
gaga <23i03g@mail.ru>
2019-12-24 18:57:57 +0400

    Update UsefulApps.cs

--- Assets/Systems/MarketSystems/Content/UsefulApps.cs
+++ Assets/Systems/MarketSystems/Content/UsefulApps.cs
@@ -20,31 +20,45 @@ public partial class MarketInitializerSystem : IInitializeSystem
             //NicheType.Qol_MusicPlayers, // small-average
             //NicheType.Qol_VideoPlayers, // average
 
-            //NicheType.Qol_Encyclopedia,
+            NicheType.Qol_Encyclopedia,
             //NicheType.Qol_Antivirus,
 
-            //NicheType.Qol_OnlineEducation
+            NicheType.Qol_OnlineEducation
         };
         AttachNichesToIndustry(IndustryType.WorkAndLife, niches);
 
-        var smallUtil = new MarketProfile(AudienceSize.Million, Monetisation.Adverts, Margin.Low, AppComplexity.Solo, NicheSpeed.Year);
+        var smallUtil =
+            new MarketProfile(AudienceSize.Million, Monetisation.Adverts, Margin.Low, AppComplexity.Solo, NicheSpeed.Year);
 
-        var officeDocumentEditor = new MarketProfile(AudienceSize.Million100, Monetisation.Service, Margin.Mid, AppComplexity.Average, NicheSpeed.Year);
+        var encyclopedia =
+            new MarketProfile(AudienceSize.Million100, Monetisation.Adverts, Margin.Mid, AppComplexity.Easy, NicheSpeed.Year);
 
-        SetMarkets(NicheType.Qol_PdfReader,             2000, 2030, officeDocumentEditor);
+        var officeDocumentEditor =
+            new MarketProfile(AudienceSize.Million100, Monetisation.Service, Margin.Mid, AppComplexity.Average, NicheSpeed.Year);
+
+        var education =
+            new MarketProfile(AudienceSize.Million100, Monetisation.Service, Margin.Mid, AppComplexity.Average, NicheSpeed.Year);
+
 
         SetMarkets(NicheType.Qol_DocumentEditors,       1980, 2040, officeDocumentEditor);
         SetMarkets(NicheType.Qol_GraphicalEditor,       1990, 2040, officeDocumentEditor);
 
-        SetMarkets(NicheType.Qol_3DGraphicalEditor,     2000, 2050, officeDocumentEditor);
-        SetMarkets(NicheType.Qol_VideoEditingTool,      2000, 2050, officeDocumentEditor);
+        SetMarkets(NicheType.Qol_3DGraphicalEditor,     1990, 2050, officeDocumentEditor);
 
 
-        SetMarkets(NicheType.Qol_AdBlocker,             2003, 2040, smallUtil);
         SetMarkets(NicheType.Qol_DiskFormattingUtils,   1999, 2030, smallUtil);
         SetMarkets(NicheType.Qol_RSSReader,             1999, 2030, smallUtil);
         SetMarkets(NicheType.Qol_Archivers,             1999, 2030, smallUtil);
 
+        SetMarkets(NicheType.Qol_PdfReader,             2000, 2030, officeDocumentEditor);
+        SetMarkets(NicheType.Qol_VideoEditingTool,      2000, 2050, officeDocumentEditor);
 
+
+        SetMarkets(NicheType.Qol_AdBlocker,             2003, 2040, smallUtil);
+        //SetMarkets(NicheType.Qol_MusicPlayers,          2003, 2040, smallUtil);
+        //SetMarkets(NicheType.Qol_VideoPlayers,          2003, 2040, smallUtil);
+        SetMarkets(NicheType.Qol_Encyclopedia,          2002, 2050, encyclopedia);
+        //SetMarkets(NicheType.Qol_Antivirus,             2003, 2040, smallUtil);
+        SetMarkets(NicheType.Qol_OnlineEducation,       2008, 2040, smallUtil);
     }
 }
