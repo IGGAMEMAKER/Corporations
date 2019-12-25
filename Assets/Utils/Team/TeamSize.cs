123f80732b095c554341d7d58f9e42898bdbb94d
gaga <23i03g@mail.ru>
2019-12-26 02:29:47 +0400

    Update TeamSize.cs

--- Assets/Utils/Team/TeamSize.cs
+++ Assets/Utils/Team/TeamSize.cs
@@ -6,95 +6,6 @@ namespace Assets.Utils
 {
     public static partial class TeamUtils
     {
-        public static int GetTeamMaxSize(GameEntity company)
-        {
-            switch (company.team.TeamStatus)
-            {
-                case TeamStatus.Solo:
-                    return 1;
-
-                case TeamStatus.Pair: return 2;
-
-                case TeamStatus.SmallTeam: return 5;
-
-                case TeamStatus.BigTeam: return 20;
-
-                default: return 100; // 11 + GetManagers(company) * 7;
-            }
-        }
-
-        public static long GetSalary(WorkerRole workerRole)
-        {
-            // TODO GET PROPER SALARIES FOR ALL ROLES
-
-            switch (workerRole)
-            {
-                case WorkerRole.Business: return Constants.SALARIES_CEO;
-                case WorkerRole.Manager: return Constants.SALARIES_MANAGER;
-                case WorkerRole.Marketer: return Constants.SALARIES_MARKETER;
-
-                case WorkerRole.ProductManager: return Constants.SALARIES_PRODUCT_PROJECT_MANAGER;
-                case WorkerRole.ProjectManager: return Constants.SALARIES_PRODUCT_PROJECT_MANAGER;
-
-                default: return Constants.SALARIES_DIRECTOR;
-            }
-        }
-
-        internal static int GetAverageTeamStrength(GameContext gameContext, GameEntity company)
-        {
-            var strength = 0;
-            var size = 0;
-            foreach (var w in company.team.Workers)
-            {
-                var worker = HumanUtils.GetHumanById(gameContext, w.Key);
-
-                strength += HumanUtils.GetWorkerRatingInRole(worker);
-                size++;
-            }
-
-            if (size == 0)
-                return 0;
-
-            return strength / size;
-        }
-
-        internal static int GetTeamRating(GameContext gameContext, GameEntity company)
-        {
-            switch (company.team.TeamStatus)
-            {
-                case TeamStatus.Solo: return 1;
-                case TeamStatus.Pair: return 2;
-                case TeamStatus.SmallTeam: return 3;
-                case TeamStatus.BigTeam: return 4;
-                case TeamStatus.Department: return 5;
-            }
-
-            var strength = GetAverageTeamStrength(gameContext, company);
-
-            var min = 45f;
-            var max = 90f;
-
-            var diff = max - min;
-
-
-            // 9 per star
-
-            float relativeStrength = Mathf.Clamp(strength, min, max);
-
-            var percent = (relativeStrength - min) / diff;
-
-            var maxStars = 5;
-
-            var rating = maxStars * percent;
-
-            return (int)Mathf.Clamp(rating, 1, maxStars);
-        }
-
-        //public static GameEntity[] GetSpecialistsByRole(GameEntity company, WorkerRole workerRole, GameContext gameContext)
-        //{
-        //    company.team.Workers.Values.ToArray().Where(w => w == workerRole);
-        //}
-
         public static int CountSpecialists(GameEntity company, WorkerRole workerRole)
         {
             return company.team.Workers.Values.ToArray().Count(w => w == workerRole);
@@ -124,16 +35,6 @@ namespace Assets.Utils
         public static int GetMarketers(GameEntity company)
         {
             return CountSpecialists(company, WorkerRole.Marketer);
-        }
-
-        public static int GetTeamSize(GameEntity company)
-        {
-            return company.team.Workers.Count();
-        }
-
-        public static bool IsWillOverextendTeam(GameEntity company)
-        {
-            return GetTeamSize(company) >= GetTeamMaxSize(company);
         }
     }
 }
