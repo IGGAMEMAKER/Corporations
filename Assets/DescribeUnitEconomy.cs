3d0dac338278190d9af9c0e7e1a840ff16250821
gaga <23i03g@mail.ru>
2020-01-09 19:51:43 +0400

    dev screen

--- Assets/DescribeUnitEconomy.cs
+++ Assets/DescribeUnitEconomy.cs
@@ -4,12 +4,24 @@ public class DescribeUnitEconomy : ParameterView
 {
     public override string RenderValue()
     {
-        var isProfitable = Economy.IsProfitable(GameContext, SelectedCompany);
+        var product = SelectedCompany;
+
+        var ads = Markets.GetClientAcquisitionCost(product.product.Niche, GameContext) * 1000;
+        var income = Economy.GetSegmentPrice(GameContext, product, 0) * 1000;
+
+        var change = income - ads;
+
+        //var isProfitable = Economy.IsProfitable(GameContext, SelectedCompany);
+        var isProfitable = change >= 0;
 
         // Your business will be " + Visuals.Positive("profitable") + ". 
+        //if (isProfitable)
+        //    return "Unit economy is " + Visuals.Positive("OK") + "\nGet as more clients as you can!";
+        //else
+        //    return "Unit economy is " + Visuals.Negative("Bad") + "\nAdd more features to succeed!";
         if (isProfitable)
-            return "Unit economy is " + Visuals.Positive("OK") + "\nGet as more clients as you can!";
+            return "Get as more clients as you can!";
         else
-            return "Unit economy is " + Visuals.Negative("Bad") + "\nAdd more features to succeed!";
+            return "Add more features to succeed!";
     }
 }
---
+++ Assets/RenderUnitEconomy.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderUnitEconomy : ParameterView
+{
+    public override string RenderValue()
+    {
+        var product = SelectedCompany;
+
+        var ads = Markets.GetClientAcquisitionCost(product.product.Niche, GameContext) * 1000;
+        var income = Economy.GetSegmentPrice(GameContext, product, 0) * 1000;
+
+        var change = income - ads;
+
+        Colorize(change >= 0 ? VisualConstants.COLOR_POSITIVE : VisualConstants.COLOR_NEGATIVE);
+
+        return change.ToString("0.0") + "$";
+    }
+}
