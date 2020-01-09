550cbf680f4658cb396256a743c039ef5ece37b1
gaga <23i03g@mail.ru>
2020-01-09 19:51:36 +0400

    iterate over daughter product companie

---
+++ Assets/HideNextCompanyButtonIfHaveOneCompany.cs
@@ -0,0 +1,11 @@
+﻿using Assets.Core;
+
+public class HideNextCompanyButtonIfHaveOneCompany : HideOnSomeCondition
+{
+    public override bool HideIf()
+    {
+        var companies = Companies.GetDaughterProductCompanies(GameContext, MyCompany);
+
+        return companies.Length <= 1;
+    }
+}
---
+++ Assets/NavigateToNextProduct.cs
@@ -0,0 +1,11 @@
+﻿using Assets.Core;
+
+public class NavigateToNextProduct : IterateOverCompaniesButtonController
+{
+    public override GameEntity[] GetEntities()
+    {
+        return Companies.GetDaughterProductCompanies(GameContext, MyCompany);
+    }
+
+    public override ScreenMode GetScreenMode() => ScreenMode.DevelopmentScreen;
+}
