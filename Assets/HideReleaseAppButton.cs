6c14ef093387df2115e8df5fc25de9f5aa0de1ba
gaga <23i03g@mail.ru>
2020-01-10 14:59:09 +0400

    move release app button to Dev screen

---
+++ Assets/HideReleaseAppButton.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideReleaseAppButton : HideOnSomeCondition
+{
+    public override bool HideIf()
+    {
+        var p = SelectedCompany;
+        return !Companies.IsReleaseableApp(p, GameContext);
+    }
+}
--- Assets/RenderMarketButtons.cs
+++ Assets/RenderMarketButtons.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class RenderMarketButtons : View
 {
     public GameObject RaiseInvestments;
-    public GameObject Release;
     public GameObject Partnerships;
     public GameObject Expand;
 
@@ -15,22 +14,22 @@ public class RenderMarketButtons : View
     {
         base.ViewRender();
 
+        bool IsMarketResearched = Markets.IsExploredMarket(GameContext, SelectedNiche);
+
+
         var amountOfCompanies = Companies.GetDaughterCompaniesAmount(MyCompany, GameContext);
         var daughtersOnMarket = Companies.GetDaughterCompaniesOnMarket(MyCompany, SelectedNiche, GameContext);
 
-        bool hasReleasebleApps  = daughtersOnMarket.Where(p => Companies.IsReleaseableApp(p, GameContext)).Count() > 0;
         bool hasReleasedApps    = daughtersOnMarket.Where(p => p.isRelease).Count() > 0;
 
         bool hasDaughtersOnMarket = daughtersOnMarket.Count() > 0;
 
-        bool IsMarketResearched = Markets.IsExploredMarket(GameContext, SelectedNiche);
 
-        bool isTimeToExpand = CurrentIntDate > 90;
 
         RaiseInvestments.SetActive(IsMarketResearched && amountOfCompanies == 1 && hasDaughtersOnMarket);
         Partnerships    .SetActive(IsMarketResearched && amountOfCompanies == 1 && hasDaughtersOnMarket && hasReleasedApps);
 
-        Release         .SetActive(IsMarketResearched && hasReleasebleApps);
+        bool isTimeToExpand = CurrentIntDate > 90;
         Expand          .SetActive(IsMarketResearched && !MyCompany.isWantsToExpand && isTimeToExpand);
     }
 }
