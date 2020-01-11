29e0999fba34285ec6e9b99b81bbdd8a53a8bdba
gaga <23i03g@mail.ru>
2020-01-11 09:35:39 +0400

    dev screen

--- Assets/RenderFeatures.cs
+++ Assets/RenderFeatures.cs
@@ -8,6 +8,9 @@ public class RenderFeatures : ParameterView
 
         var freeFeatures = Products.GetFreeImprovements(product);
 
+        if (Cooldowns.IsHasTask(GameContext, new CompanyTaskUpgradeFeature(product.company.Id, ProductImprovement.Acquisition)))
+            freeFeatures -= 1;
+
         return freeFeatures.ToString();
     }
 }
