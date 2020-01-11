5959e01a80239fde6af6f35d06b22e9fab7af81c
gaga <23i03g@mail.ru>
2020-01-11 06:55:07 +0400

    hide improvement buttons if app is ready for release

--- Assets/HideIfHasNoImprovements.cs
+++ Assets/HideIfHasNoImprovements.cs
@@ -4,10 +4,15 @@ public class HideIfHasNoImprovements : HideOnSomeCondition
 {
     public override bool HideIf()
     {
-        return !Companies.IsRelatedToPlayer(GameContext, SelectedCompany)
-            || !Products.HasFreeImprovements(SelectedCompany)
+        var p = SelectedCompany;
+
+        return !Companies.IsRelatedToPlayer(GameContext, p)
+            || !Products.HasFreeImprovements(p)
             
             // ProductImprovement.Acquisition is not required
-            || Cooldowns.IsHasTask(GameContext, new CompanyTaskUpgradeFeature(SelectedCompany.company.Id, ProductImprovement.Acquisition));
+            || Cooldowns.IsHasTask(GameContext, new CompanyTaskUpgradeFeature(p.company.Id, ProductImprovement.Acquisition))
+            
+            || Companies.IsReleaseableApp(p, GameContext);
+            ;
     }
 }
