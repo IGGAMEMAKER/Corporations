d017eed8ca0bbabcb5a4881263be68c2607d24cd
gaga <23i03g@mail.ru>
2020-01-11 06:55:15 +0400

    dev

---
+++ Assets/RenderProductActivities.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RenderProductActivities : ParameterView
+{
+    public override string RenderValue()
+    {
+        var tasks = Cooldowns.GetTasksOfCompany(GameContext, SelectedCompany.company.Id);
+
+        var count = tasks.Count();
+
+        if (count > 0)
+            return $"Current activities ({count})";
+        else
+            return "";
+    }
+}
