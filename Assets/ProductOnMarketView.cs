699adb655151c69c40f5bb4226b780ef18c340dd
gaga <23i03g@mail.ru>
2020-01-25 17:47:44 +0400

    reworking Niche Screen

---
+++ Assets/ProductOnMarketView.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProductOnMarketView : View
+{
+    public Text Clients;
+    public Text NewClients;
+    public Hint ClientChange;
+
+    public Text Name;
+
+    int companyId;
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+        //Clients.text = 
+    }
+
+    void Render()
+    {
+        var company = Companies.GetCompany(GameContext, companyId);
+
+        var clients = MarketingUtils.GetClients(company);
+        var newClients = 15000;
+
+        Clients.text = Format.Minify(clients);
+        NewClients.text = Format.Minify(newClients);
+
+        Name.text = company.company.Name;
+
+        ClientChange.SetHint($"{company.company.Name} will get this amount of clients next week");
+    }
+
+    public void SetEntity(int companyId)
+    {
+        this.companyId = companyId;
+
+        Render();
+    }
+}
---
+++ Assets/ProductsOnMarketList.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ProductsOnMarketList : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        var c = entity as GameEntity;
+        t.GetComponent<ProductOnMarketView>().SetEntity(c.company.Id);
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var products = Markets.GetProductsOnMarket(GameContext, SelectedNiche)
+            .OrderByDescending(p => MarketingUtils.GetClients(p))
+            ;
+
+        SetItems(products);
+    }
+}
