fee606a021a7043e171013d48e37f5a239e4acac
gaga <23i03g@mail.ru>
2020-02-21 01:29:07 +0400

    project screen

---
+++ Assets/RenderEconomyTabName.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderEconomyTabName : UpgradedParameterView
+{
+    public override string RenderHint() => "";
+
+    public override string RenderValue()
+    {
+        var cost = Economy.GetCompanyCost(Q, SelectedCompany);
+
+        return $"Economy ({Format.MinifyMoney(cost)})";
+    }
+}
