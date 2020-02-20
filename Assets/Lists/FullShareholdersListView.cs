d9b3a31528ece1ffb2c86286f7b5e38841c020b5
gaga <23i03g@mail.ru>
2020-02-21 03:27:41 +0400

    project screen: shareholders

--- Assets/Lists/FullShareholdersListView.cs
+++ Assets/Lists/FullShareholdersListView.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Assets.Core;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FullShareholdersListView : ListView
@@ -9,5 +11,15 @@ public class FullShareholdersListView : ListView
 
         t.GetComponent<ShareholderView>()
             .SetEntity(e.Key, e.Value);
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var shareholders = Companies.GetShareholders(SelectedCompany)
+            .OrderByDescending(s => Companies.GetAmountOfShares(Q, SelectedCompany.company.Id, s.Key));
+
+        SetItems(shareholders);
     }
 }
