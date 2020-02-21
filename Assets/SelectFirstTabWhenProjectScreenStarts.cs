474d826286ede8688c982f641a793f7c2b11ad5e
gaga <23i03g@mail.ru>
2020-02-21 18:42:22 +0400

    almost fixed project screen bug

---
+++ Assets/SelectFirstTabWhenProjectScreenStarts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Michsky.UI.Frost;
+
+public class SelectFirstTabWhenProjectScreenStarts : MonoBehaviour
+{
+    public TopPanelManager TopPanelManager;
+
+    void OnEnable()
+    {
+        TopPanelManager.PanelAnim(0);
+    }
+}
