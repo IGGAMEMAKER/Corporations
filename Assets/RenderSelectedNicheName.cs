358e081326cf7829e460f841ef6a1db0fc1264b7
gaga <23i03g@mail.ru>
2020-02-21 19:18:36 +0400

    consolidated market screen

--- Assets/RenderSelectedNicheName.cs
+++ Assets/RenderSelectedNicheName.cs
@@ -1,7 +1,9 @@
 ﻿using Assets.Core;
 
-public class RenderSelectedNicheName : ParameterView
+public class RenderSelectedNicheName : UpgradedParameterView
 {
+    public override string RenderHint() => "";
+
     public override string RenderValue()
     {
         return "Market of " + Enums.GetFormattedNicheName(ScreenUtils.GetSelectedNiche(Q));
