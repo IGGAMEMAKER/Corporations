97387ca5bc02d67a5d1bdace1a1d49a197b6d73d
gaga <23i03g@mail.ru>
2020-02-24 01:23:44 +0400

    struggling with sorting order

--- Assets/CanvasSortingOrderFix.cs
+++ Assets/CanvasSortingOrderFix.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanvasSortingOrderFix : View
+public class CanvasSortingOrderFix : MonoBehaviour
 {
     public Canvas Canvas;
 
-    public override void ViewRender()
+    void Start()
     {
-        base.ViewRender();
-
-        Canvas.sortingOrder = -90;
+        Canvas.enabled = false;
     }
+
+    void Update()
+    {
+        //StartCoroutine(ExecuteAfterTime(0.01f));
+        Canvas.enabled = true;
+    }
+
+    //IEnumerator ExecuteAfterTime(float time)
+    //{
+    //    //yield return new WaitForSeconds(time);
+    //    //Canvas.enabled = false;
+    //    Canvas.enabled = true;
+
+    //    //Destroy(this);
+    //}
 }
