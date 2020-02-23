6e828bf5d4884b8cca669e3a64c51875431f895e
gaga <23i03g@mail.ru>
2020-02-24 00:47:35 +0400

    fixing right panel sorting order bug

---
+++ Assets/CanvasSortingOrderFix.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasSortingOrderFix : View
+{
+    public Canvas Canvas;
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        Canvas.sortingOrder = -90;
+    }
+}
