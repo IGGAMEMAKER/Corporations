50dca6528314363ec93333f8af11ae9f2e6d44de
gaga <23i03g@mail.ru>
2020-02-25 14:10:26 +0400

    fix

--- Assets/Relays/HideFlagshipLabelIfOnlyOneCompany.cs
+++ Assets/Relays/HideFlagshipLabelIfOnlyOneCompany.cs
@@ -4,6 +4,6 @@ public class HideFlagshipLabelIfOnlyOneCompany : HideOnSomeCondition
 {
     public override bool HideIf()
     {
-        return Companies.GetDaughterCompaniesAmount(MyCompany, Q) < 2;
+        return Companies.GetDaughterCompaniesAmount(MyCompany, Q) == 0;
     }
 }
