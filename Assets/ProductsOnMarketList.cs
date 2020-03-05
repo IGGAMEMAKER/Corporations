639630b7b99f5a5d22415f67ffad3d931b009fe8
gaga <23i03g@mail.ru>
2020-03-05 22:31:39 +0400

    Update ProductsOnMarketList.cs

--- Assets/ProductsOnMarketList.cs
+++ Assets/ProductsOnMarketList.cs
@@ -11,7 +11,7 @@ public class ProductsOnMarketList : ListView
         var c = entity as GameEntity;
 
         var maxClients = Markets.GetProductsOnMarket(Q, SelectedNiche)
-    .Max(Marketing.GetClients);
+            .Max(Marketing.GetClients);
 
         t.GetComponent<ProductOnMarketView>().SetEntity(c.company.Id, maxClients);
     }
