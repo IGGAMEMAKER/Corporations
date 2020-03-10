f9266e92861846884b3e769a6e6ce4e436fd2e22
gaga <23i03g@mail.ru>
2020-03-10 11:12:34 +0400

    Update BrandingCampaign.cs

--- Assets/Utils/Marketing/Campaigns/BrandingCampaign.cs
+++ Assets/Utils/Marketing/Campaigns/BrandingCampaign.cs
@@ -7,7 +7,7 @@
             var clientCost = Markets.GetClientAcquisitionCost(product.product.Niche, gameContext);
             var flow = GetClientFlow(gameContext, product.product.Niche);
 
-            var cost = clientCost * flow * 10;
+            var cost = clientCost * flow * 5;
 
             var discount = GetCorpCultureMarketingDiscount(product, gameContext);
 
