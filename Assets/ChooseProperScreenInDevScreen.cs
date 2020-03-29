4c833c45127fda01082cf97f308cdbf8d16faaaf
gaga <23i03g@mail.ru>
2020-03-29 16:48:11 +0400

    splitting main screen in 4 screens: non released phase, released phase, industrial phase and mission phase

---
+++ Assets/ChooseProperScreenInDevScreen.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseProperScreenInDevScreen : View
+{
+    public GameObject NonReleasedScreen;
+    public GameObject ReleasedScreen;
+    public GameObject IndustrialScreen;
+    public GameObject MissionScreen;
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        bool hasReleasedProducts = Companies.IsHasReleasedProducts(Q, MyCompany);
+        var numberOfDaughters = Companies.GetDaughterCompaniesAmount(MyCompany, Q);
+
+        NonReleasedScreen.SetActive(!hasReleasedProducts);
+
+        ReleasedScreen.SetActive(numberOfDaughters == 1 && hasReleasedProducts);
+        // TODO also check if products are in same industry
+        IndustrialScreen.SetActive(numberOfDaughters > 1 && hasReleasedProducts);
+
+        // check company goal here
+        MissionScreen.SetActive(false);
+    }
+}
