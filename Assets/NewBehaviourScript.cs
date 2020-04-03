e22149858ae34b593131d702ceaa13085837272b
gaga <23i03g@mail.ru>
2020-04-04 03:27:59 +0400

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [MenuItem("Hugh/Mongous _F8")]
+    static void DoSomething()
+    {
+        Debug.Log("Humongous what?");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
