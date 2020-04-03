cdcc6b409af3b9b2b34c38e2a640da728bc73b4a
gaga <23i03g@mail.ru>
2020-04-04 03:42:20 +0400

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -10,16 +10,4 @@ public class NewBehaviourScript : MonoBehaviour
     {
         Debug.Log("Humongous what?");
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
