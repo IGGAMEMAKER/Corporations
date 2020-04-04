92b2d9f98763aff14e0981bf16c6c3e4440cce9f
gaga <23i03g@mail.ru>
2020-04-04 19:15:12 +0400

    Semi-Automatic new component move to Views folder done!

--- Assets/Editor/FileImportHandler.cs
+++ Assets/Editor/FileImportHandler.cs
@@ -9,13 +9,14 @@ using System.IO;
 //purpose of this postprocessor is to ensure that listed file extensions
 // are not in certain filepaths, when they are they are moved to a 
 //specified default path
-public class FileImportHandler : AssetPostprocessor
+public class FileImportHandler :
+    AssetPostprocessor
 {
     //only evaluate files imported into these paths
     static List<string> pathsToMoveFrom = new List<string>()
-     {
-         "Assets"
-     };
+    {
+        "Assets"
+    };
 
 
     static Dictionary<string, string> defaultFileLocationByExtension = new Dictionary<string, string>()
@@ -38,15 +39,20 @@ public class FileImportHandler : AssetPostprocessor
          ////like images, there are sub folders that the user must manage
          //{".wav",    "Assets/Audio/Sources"}, 
          //like images, there are sub folders that the user must manage
-         {".cs",     "Assets/Views/Temp/"},
+         {".cs",     "Assets/Views/"},
          //{".shader", "Assets/Dev/Shaders"},
          //{".cginc",  "Assets/Dev/Shaders"}
      };
 
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
     {
+        List<string> oldFilePaths = new List<string>();
+        List<string> newFilePaths = new List<string>();
+
         foreach (string oldFilePath in importedAssets)
         {
+            Debug.Log("Checking path: " + oldFilePath);
+
             string directory = Path.GetDirectoryName(oldFilePath);
             if (!pathsToMoveFrom.Contains(directory))
                 continue;
@@ -58,10 +64,16 @@ public class FileImportHandler : AssetPostprocessor
             string filename = Path.GetFileName(oldFilePath);
             string newPath = defaultFileLocationByExtension[extension];
 
-            AssetDatabase.MoveAsset(oldFilePath, newPath + filename);
+            oldFilePaths.Add(oldFilePath);
+            newFilePaths.Add(newPath + filename);
+            //var str = AssetDatabase.MoveAsset(oldFilePath, newPath + filename);
 
+            Debug.Log(string.Format("Moving asset ({0}) from {2} to path: {1}", filename, newPath, oldFilePath));
+        }
 
-            Debug.Log(string.Format("Moving asset ({0}) to path: {1}", filename, newPath));
+        for (var i = 0; i < oldFilePaths.Count; i++)
+        {
+            File.Move(oldFilePaths[i], newFilePaths[i]);
         }
     }
 }
