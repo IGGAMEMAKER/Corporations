814ea87c61d7cf4478c6a682c3820106b00b85d7
gaga <23i03g@mail.ru>
2020-04-09 18:04:22 +0400

    meta files

---
+++ Assets/VisualStudioProjectGenerationPostProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+public class VisualStudioProjectGenerationPostProcess : AssetPostprocessor
+{
+    private static void OnGeneratedCSProjectFiles()
+    {
+        Debug.Log("OnGeneratedCSProjectFiles");
+        var dir = Directory.GetCurrentDirectory();
+        var files = Directory.GetFiles(dir, "*.csproj");
+        foreach (var file in files)
+            ChangeTargetFrameworkInfProjectFiles(file);
+    }
+
+    static void ChangeTargetFrameworkInfProjectFiles(string file)
+    {
+        var text = File.ReadAllText(file);
+        var find = "TargetFrameworkVersion>v4.6</TargetFrameworkVersion";
+        var replace = "TargetFrameworkVersion>v4.7</TargetFrameworkVersion";
+
+        if (text.IndexOf(find) != -1)
+        {
+            text = Regex.Replace(text, find, replace);
+            File.WriteAllText(file, text);
+        }
+    }
+
+}
