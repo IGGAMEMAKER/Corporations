b48151acd55ee369799531d4f040a43d9d700ffe
gaga <23i03g@mail.ru>
2020-04-20 01:37:52 +0400

    Important: Views Assembly

--- Assets/Editor/VisualStudioProjectGenerationPostProcess.cs
+++ Assets/Editor/VisualStudioProjectGenerationPostProcess.cs
@@ -8,34 +8,33 @@ using UnityEngine;
 // target migrator
 // https://stackoverflow.com/questions/28436066/retargeting-all-projects-in-a-solution-to-net-4-5-2
 
-#if (UNITY_EDITOR)
-public class VisualStudioProjectGenerationPostProcess : AssetPostprocessor
-{
-    private static void OnGeneratedCSProjectFiles()
-    {
-        Debug.Log("-------------------------");
-        Debug.Log("OnGeneratedCSProjectFiles");
-        Debug.Log("-------------------------");
+//#if (UNITY_EDITOR)
+//public class VisualStudioProjectGenerationPostProcess : AssetPostprocessor
+//{
+//    private static void OnGeneratedCSProjectFiles()
+//    {
+//        Debug.Log("OnGeneratedCSProjectFiles");
+//        Debug.Log("-------------------------");
 
-        //var dir = Directory.GetCurrentDirectory();
-        //var files = Directory.GetFiles(dir, "*.csproj");
-        //foreach (var file in files)
-        //    ChangeTargetFrameworkInfProjectFiles(file);
-    }
+//        //var dir = Directory.GetCurrentDirectory();
+//        //var files = Directory.GetFiles(dir, "*.csproj");
+//        //foreach (var file in files)
+//        //    ChangeTargetFrameworkInfProjectFiles(file);
+//    }
 
-    static void ChangeTargetFrameworkInfProjectFiles(string file)
-    {
-        Debug.Log("Changing Target Framework for: " + file);
+//    static void ChangeTargetFrameworkInfProjectFiles(string file)
+//    {
+//        Debug.Log("Changing Target Framework for: " + file);
 
-        var text = File.ReadAllText(file);
-        var find = "TargetFrameworkVersion>*</TargetFrameworkVersion";
-        var replace = "TargetFrameworkVersion>v4.7.2</TargetFrameworkVersion";
+//        var text = File.ReadAllText(file);
+//        var find = "TargetFrameworkVersion>*</TargetFrameworkVersion";
+//        var replace = "TargetFrameworkVersion>v4.7.2</TargetFrameworkVersion";
 
-        if (text.IndexOf(find) != -1)
-        {
-            text = Regex.Replace(text, find, replace);
-            File.WriteAllText(file, text);
-        }
-    }
-}
-#endif
+//        if (text.IndexOf(find) != -1)
+//        {
+//            text = Regex.Replace(text, find, replace);
+//            File.WriteAllText(file, text);
+//        }
+//    }
+//}
+//#endif
