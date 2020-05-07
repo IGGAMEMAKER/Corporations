249d380a51ff569a8561fd7cac7ef3742833c2c1
gaga <23i03g@mail.ru>
2020-05-07 20:21:44 +0400

    Update HideMainSceneOnGameStart.cs

--- Assets/HideMainSceneOnGameStart.cs
+++ Assets/HideMainSceneOnGameStart.cs
@@ -13,18 +13,20 @@ public class HideMainSceneOnGameStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
+        //SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
         SceneManager.sceneLoaded += SceneManager_sceneLoaded;
     }
 
     private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
-        Canvas.SetActive(false);
+        if (Canvas != null)
+            Canvas.SetActive(false);
     }
 
     private void SceneManager_activeSceneChanged(Scene arg0, Scene arg1)
     {
-        Canvas.SetActive(false);
+        if (Canvas != null)
+            Canvas.SetActive(false);
     }
 }
 
