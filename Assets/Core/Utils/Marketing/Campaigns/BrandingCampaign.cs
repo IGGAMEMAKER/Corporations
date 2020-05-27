95a4dd0a93071b39a0f48d1eeae88a43565f75a6
gaga <23i03g@mail.ru>
2020-05-27 23:02:55 +0400

    restructure

--- Assets/Core/Utils/Marketing/Campaigns/BrandingCampaign.cs
+++ Assets/Core/Utils/Marketing/Campaigns/BrandingCampaign.cs
@@ -15,5 +15,17 @@
 
             return (long)result;
         }
+
+        public static void ToggleChannelActivity(GameEntity product, GameContext gameContext, GameEntity channel)
+        {
+            var companyId = product.company.Id;
+
+            var active = channel.companyMarketingActivities.Companies.ContainsKey(companyId);
+
+            if (active)
+                channel.companyMarketingActivities.Companies.Remove(companyId);
+            else
+                channel.companyMarketingActivities.Companies[companyId] = 1;
+        }
     }
 }
---
+++ Assets/Views/ChannelActivityController.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChannelActivityController : ButtonController
+{
+    public MarketingChannelView MarketingChannelView;
+    public override void Execute()
+    {
+        var channel = MarketingChannelView.channel;
+
+        Marketing.ToggleChannelActivity(Flagship, Q, channel);
+
+        MarketingChannelView.ViewRender();
+    }
+}
