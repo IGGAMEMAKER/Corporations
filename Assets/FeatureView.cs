a13ffd199d9d2c28c5d51d16b01efcf9955f8e08
gaga <23i03g@mail.ru>
2020-06-05 18:13:15 +0400

    feature mockup rendering

--- Assets/FeatureView.cs
+++ Assets/FeatureView.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using Assets.Core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FeatureView : View
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text Name;
+    public Text Benefits;
+    public Text Rating;
+
+    public NewProductFeature NewProductFeature;
+
+    public override void ViewRender()
     {
-        
+        base.ViewRender();
+
+        if (NewProductFeature == null)
+            return;
+
+        Name.text = NewProductFeature.Name;
+        Benefits.text = GetFeatureBenefits();
+
+        var rating = Random.Range(0, 10f);
+
+        Rating.text = rating.ToString("0.0");
+        Rating.color = Visuals.GetGradientColor(0, 10f, rating);
     }
 
-    // Update is called once per frame
-    void Update()
+    public string GetFeatureBenefits()
     {
-        
+        var b = NewProductFeature.FeatureBonus;
+
+        if (b is FeatureBonusAcquisition)
+            return $"+{b.Max}% user gain";
+
+        if (b is FeatureBonusMonetisation)
+            return $"+{b.Max}% income";
+
+        if (b is FeatureBonusRetention)
+            return $"-{b.Max}% client loss";
+
+        return b.GetType().ToString();
+    }
+
+    public void SetFeature(NewProductFeature newProductFeature)
+    {
+        NewProductFeature = newProductFeature;
+
+        ViewRender();
     }
 }
---
+++ Assets/ProductFeaturesListView.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureBonus
+{
+    // max bonus
+    public float Max;
+
+    public FeatureBonus(float Max)
+    {
+        this.Max = Max;
+    }
+}
+
+public class FeatureBonusAcquisition : FeatureBonus
+{
+    public FeatureBonusAcquisition(float Max) : base(Max)
+    {
+    }
+}
+public class FeatureBonusRetention : FeatureBonus
+{
+    public FeatureBonusRetention(float Max) : base(Max)
+    {
+    }
+}
+public class FeatureBonusMonetisation : FeatureBonus
+{
+    public FeatureBonusMonetisation(float Max) : base(Max)
+    {
+    }
+}
+
+public class NewProductFeature
+{
+    public string Name;
+    public FeatureBonus FeatureBonus;
+}
+
+public class ProductFeaturesListView : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        t.GetComponent<FeatureView>().SetFeature(entity as NewProductFeature);
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var features = new NewProductFeature[] {
+            new NewProductFeature { Name = "Monetisation", FeatureBonus = new FeatureBonusMonetisation(5) },
+            new NewProductFeature { Name = "Retention", FeatureBonus = new FeatureBonusRetention(5) },
+            new NewProductFeature { Name = "Start Page", FeatureBonus = new FeatureBonusAcquisition(5) },
+        };
+
+        SetItems(features);
+    }
+}
