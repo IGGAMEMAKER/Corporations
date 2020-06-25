9151862d381466e470c297cbc13108dc0b228927
gaga <23i03g@mail.ru>
2020-06-26 01:37:58 +0400

    Update RenderChurnRate.cs

--- Assets/Views/RenderChurnRate.cs
+++ Assets/Views/RenderChurnRate.cs
@@ -4,7 +4,17 @@ public class RenderChurnRate : UpgradedParameterView
 {
     public override string RenderHint()
     {
-        return Marketing.GetChurnBonus(Q, company.company.Id).ToString(true);
+        var bonus = new Bonus<long>("Audience change");
+
+        var churnBonus = Marketing.GetChurnBonus(Q, company.company.Id);
+        churnBonus.SetDimension("%");
+        var churnUsers = Marketing.GetChurnClients(Q, company.company.Id);
+
+        bonus.Append("Growth", Marketing.GetAudienceGrowth(company, Q));
+        bonus.Append("Loss", -churnUsers);
+        bonus.MinifyValues();
+
+        return Visuals.Positive(bonus.ToString()) + "\n" + churnBonus.ToString(true);
     }
 
     GameEntity company => Flagship;
@@ -14,9 +24,20 @@ public class RenderChurnRate : UpgradedParameterView
         var rate = Marketing.GetChurnRate(Q, company.company.Id).ToString();
         var churnUsers = Marketing.GetChurnClients(Q, company.company.Id);
 
-        Colorize(Colors.COLOR_NEGATIVE);
+        var growth = Marketing.GetAudienceGrowth(company, Q);
 
-        return Format.Minify(churnUsers);
+        var change = growth - churnUsers;
+
+        Colorize(Visuals.GetColorPositiveOrNegative(change));
+
+        var sign = "";
+
+        if (change > 0)
+            sign = "+";
+
+
+
+        return sign + Format.Minify(change);
 
         //return $"{Format.Minify(churnUsers)} users ({rate}%)";
         //return MarketingUtils.GetChurnBonus(GameContext, SelectedCompany.company.Id).Sum();
