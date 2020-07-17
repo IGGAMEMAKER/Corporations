ca475563b059cda9c049b9358b45da202568b0e7
gaga <23i03g@mail.ru>
2020-07-17 23:06:04 +0400

    improving core gameplay further

---
+++ Assets/RenderAudiencesListView.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudienceInfo
+{
+    public string Name;
+    public int Loyalty;
+    public string Needs;
+    public string Icon;
+}
+
+public class RenderAudiencesListView : ListView
+{
+    public override void SetItem<T>(Transform t, T entity, object data = null)
+    {
+        t.GetComponent<SegmentPreview>().SetEntity(entity as AudienceInfo);
+    }
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var audiences = new List<AudienceInfo>
+        {
+            new AudienceInfo { Loyalty = Random.Range(-15, 20), Name = "Teenagers",                 Needs = "Needs messaging, profiles, friends, voice chats, video chats, emojis, file sending", Icon = "Teenager" },
+            new AudienceInfo { Loyalty = Random.Range(-15, 20), Name = "Adults (20-30 years)",      Needs = "Needs messaging, profiles, friends, voice chats", Icon = "Adult" },
+            new AudienceInfo { Loyalty = Random.Range(-15, 20), Name = "Middle aged people (30+)",  Needs = "Needs messaging, profiles, friends, voice chats", Icon = "Middle" },
+            new AudienceInfo { Loyalty = Random.Range(-15, 20), Name = "Old people",                Needs = "Needs messaging, friends, voice chats, video chats", Icon = "Old" },
+        };
+
+        SetItems(audiences);
+    }
+
+    private void OnEnable()
+    {
+        ViewRender();
+    }
+}
---
+++ Assets/SegmentPreview.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SegmentPreview : View
+{
+    public Text Title;
+    public Text Description;
+    public RawImage Icon;
+
+    public void SetEntity(AudienceInfo info)
+    {
+        Title.text = $"{info.Name} <b>{Visuals.PositiveOrNegativeMinified(info.Loyalty)}</b>";
+        Description.text = info.Needs;
+
+        Icon.texture = Resources.Load($"Audiences/{info.Icon}") as Texture2D;
+    }
+}
