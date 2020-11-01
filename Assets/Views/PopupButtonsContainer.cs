2e48c738d9de110cf9b7223377c338162a952fa7
gaga <23i03g@mail.ru>
2020-11-01 21:53:52 +0400

    Update PopupButtonsContainer.cs

--- Assets/Views/PopupButtonsContainer.cs
+++ Assets/Views/PopupButtonsContainer.cs
@@ -7,7 +7,14 @@ public class PopupButtonsContainer : ListView
     public override void SetItem<T>(Transform t, T entity)
     {
         //AttachComponent(t, entity as System.Type);
-        t.gameObject.AddComponent(entity as System.Type);
+        var TT = entity as System.Type;
+
+        while (t.gameObject.GetComponents(TT).Length > 0)
+        {
+            Destroy(t.gameObject.GetComponent(TT));
+        }
+
+        t.gameObject.AddComponent(TT);
     }
 
     //void AttachComponent<T>(Transform t, T type) where T : System.Type
