ebb6a772291cdb1f815d077a5fea8ec188a1d16c
gaga <23i03g@mail.ru>
2020-11-23 00:36:15 +0400

    no product create button

---
+++ Assets/HideCreateProductButtonIfCreatedAlready.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCreateProductButtonIfCreatedAlready : View
+{
+    public GameObject CreateProductButton;
+
+    public override void ViewRender()
+    {
+        base.ViewRender();
+
+        var niche = SelectedNiche;
+
+        Draw(CreateProductButton, !Companies.HasCompanyOnMarket(MyCompany, niche, Q));
+    }
+}
