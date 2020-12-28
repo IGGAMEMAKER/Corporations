40602d602a5a36e8d0763745e1d3b34294e506c3
gaga <23i03g@mail.ru>
2020-12-28 19:25:46 +0400

    Moving back

---
+++ Assets/LoadMainGamePrefab.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMainGamePrefab : MonoBehaviour
+{
+    void Start()
+    {
+        Instantiate(Resources.Load<GameObject>("MainGameScreen"));
+        // Instantiate(Resources.Load<GameObject>("Prefabs/MainGameScreen.prefab"));
+    }
+}
