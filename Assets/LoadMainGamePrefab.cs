2985f9f34502586f2d9123ae3f7ea4ff75587d68
gaga <23i03g@mail.ru>
2021-01-05 19:22:26 +0400

    Moving SimpleUI renderer to the root of scene

--- Assets/LoadMainGamePrefab.cs
+++ Assets/LoadMainGamePrefab.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class LoadMainGamePrefab : MonoBehaviour
+public class LoadMainGamePrefab : View
 {
     void Start()
     {
-        Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/MainGameScreen.prefab"));
+        OpenUrl("/");
+        OpenUrl("/Holding/Main");
+        // Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/MainGameScreen.prefab"));
+
         // Instantiate(Resources.Load<GameObject>("Prefabs/MainGameScreen"));
         // Instantiate(Resources.Load<GameObject>("Prefabs/MainGameScreen.prefab"));
     }
