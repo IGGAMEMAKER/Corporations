15fefc0322ccc6b02e1d55dd83d3f605d17fca18
gaga <23i03g@mail.ru>
2021-01-15 13:25:35 +0400

    IMPORTANT: save before upgrade to 2020

--- Assets/Views/Upgrades/ProductUpgradeButtons.cs
+++ Assets/Views/Upgrades/ProductUpgradeButtons.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.Core;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProductUpgradeButtons : RoleRelatedButtons
 {
@@ -30,4 +32,60 @@ public class ProductUpgradeButtons : RoleRelatedButtons
 
         Render();
     }
+}
+
+// Project manager
+class CustomImage
+{
+    Image Image;
+
+    // or
+
+    string ImageUrl;
+}
+
+class CustomVideo
+{
+    string Url; // both web and desktop
+}
+
+class InterestInfo
+{
+    CustomImage Logo;
+    CustomImage MainScreen;
+    string Sentence;
+    List<CustomImage> Screenshots;
+    string Description; // abz
+    string FullDescription;
+
+    CustomVideo Trailer;
+    CustomVideo LetsPlay;
+}
+
+class RawTextData
+{
+    public List<string> Texts;
+}
+
+class ProjectInfo
+{
+    AudienceInfo AudienceInfo;
+    InterestInfo InterestInfo;
+    string WhatAreYouDoing;
+
+    string WhyWillItWork;
+    List<string> ClientSources;
+
+    List<string> DefaultQuestions = new List<string> {
+        "Holding Attention", "Getting Attention",
+        "Why wanna play", "Why wanna buy", "Why wanna feedback", "Feedback type",
+
+        "Why will they like it", "Who will be a superfan", "How to work w them",
+        "What's the challenge", "What's the game essence"
+    };
+
+    // default state
+    // empty pages
+    // 0 fans
+    List<RawTextData> Infos;
 }
