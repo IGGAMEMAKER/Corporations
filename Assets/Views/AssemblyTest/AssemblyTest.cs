2fafcd1d45174087b9895887b5810d941669f7cc
gaga <23i03g@mail.ru>
2021-01-20 19:20:44 +0400

    Compile time profiler

---
+++ Assets/Views/AssemblyTest/AssemblyTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssemblyTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
