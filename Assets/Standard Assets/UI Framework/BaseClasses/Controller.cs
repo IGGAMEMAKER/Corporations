e2b894ba422dd4390b2eb9020dbf8e185b866730
gaga <23i03g@mail.ru>
2021-05-15 18:25:27 +0400

    more restructuring

--- Assets/Standard Assets/UI Framework/BaseClasses/Controller.cs
+++ Assets/Standard Assets/UI Framework/BaseClasses/Controller.cs
@@ -3,7 +3,7 @@ using System.Runtime.Remoting.Messaging;
 using Assets.Core;
 using UnityEngine;
 
-public abstract class Controller : BaseClass
+public abstract partial class Controller : BaseClass
 {
     [Header("Always true except MenuController")]
     public bool TriggerOnEnable = true;
@@ -12,95 +12,16 @@ public abstract class Controller : BaseClass
     private View[] Views;
     private ButtonView[] buttonViews;
 
-    private static ProfilingComponent _profiler;
-
-    public static ProfilingComponent MyProfiler
-    {
-        get
-        {
-            if (_profiler == null)
-                _profiler = Companies.GetProfilingComponent(Q);
-
-            return _profiler;
-        }
-    }
-
-    public void ListenNavigationChanges(INavigationHistoryListener listener)
-    {
-        ScreenUtils.GetMenu(Q).AddNavigationHistoryListener(listener);
-    }
-
-    public void UnListenNavigationChanges(INavigationHistoryListener listener)
-    {
-        ScreenUtils.GetMenu(Q).RemoveNavigationHistoryListener(listener);
-    }
-
-
-    public void ListenMenuChanges(IMenuListener listener)
-    {
-        ScreenUtils.GetMenu(Q).AddMenuListener(listener);
-    }
-
-    public void UnListenMenuChanges(IMenuListener listener)
-    {
-        ScreenUtils.GetMenu(Q).RemoveMenuListener(listener);
-    }
-
-
-    public void ListenDateChanges(IDateListener listener)
-    {
-        ScheduleUtils.ListenDateChanges(Q, listener);
-    }
-
-    public void UnListenDateChanges(IDateListener listener)
-    {
-        ScheduleUtils.UnsubscribeFromDateChanges(Q, listener);
-    }
-
-    public GameEntity AnyChangeListener()
-    {
-        return ScreenUtils.GetMenu(Q);
-    }
-
     public void Render()
     {
         foreach (var view in Views)
-        {
-            var startTime = DateTime.Now;
-
-            // if (view.gameObject.activeSelf)
-            // {
-                view.ViewRender();
-                
-                Companies.Measure(view.name, startTime, MyProfiler, "Views");
-            // }
-
-            //if (view.name == "Gameplay")
-            //{
-            //    Debug.Log("Rendering Gameplay object");
-            //}
-        }
+            view.ViewRender();
 
         foreach (var view in buttonViews)
-        {
-            var startTime = DateTime.Now;
-
-            // if (view.gameObject.activeSelf)
-            // {
-                view.ViewRender();
-                
-                Companies.Measure(view.name, startTime, MyProfiler, "Views");
-            // }
-        }
-
-        // foreach (var view in GetComponents<View>())
-        //     view.ViewRender();
-        //
-        // foreach (var view in GetComponents<ButtonView>())
-        //     view.ViewRender();
+            view.ViewRender();
     }
 
-    private void FillListeners()
+    void FillListeners()
     {
         Views = GetComponents<View>();
         buttonViews = GetComponents<ButtonView>();
---
+++ Assets/Standard Assets/UI Framework/BaseClasses/CorporationStuff/ControllerExtension.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using Assets.Core;
+using UnityEngine;
+
+public abstract partial class Controller : BaseClass
+{
+    private static ProfilingComponent _profiler;
+
+    public static ProfilingComponent MyProfiler
+    {
+        get
+        {
+            if (_profiler == null)
+                _profiler = Companies.GetProfilingComponent(Q);
+
+            return _profiler;
+        }
+    }
+
+    void RenderAndMeasure()
+    {
+        foreach (var view in Views)
+        {
+            var startTime = DateTime.Now;
+
+            // if (view.gameObject.activeSelf)
+            // {
+            view.ViewRender();
+
+            Companies.Measure(view.name, startTime, MyProfiler, "Views");
+            // }
+        }
+
+        foreach (var view in buttonViews)
+        {
+            var startTime = DateTime.Now;
+
+            // if (view.gameObject.activeSelf)
+            // {
+            view.ViewRender();
+
+            Companies.Measure(view.name, startTime, MyProfiler, "Views");
+            // }
+        }
+    }
+
+    // listeners
+    public void ListenNavigationChanges(INavigationHistoryListener listener)
+    {
+        ScreenUtils.GetMenu(Q).AddNavigationHistoryListener(listener);
+    }
+
+    public void UnListenNavigationChanges(INavigationHistoryListener listener)
+    {
+        ScreenUtils.GetMenu(Q).RemoveNavigationHistoryListener(listener);
+    }
+
+
+    public void ListenMenuChanges(IMenuListener listener)
+    {
+        ScreenUtils.GetMenu(Q).AddMenuListener(listener);
+    }
+
+    public void UnListenMenuChanges(IMenuListener listener)
+    {
+        ScreenUtils.GetMenu(Q).RemoveMenuListener(listener);
+    }
+
+
+    public void ListenDateChanges(IDateListener listener)
+    {
+        ScheduleUtils.ListenDateChanges(Q, listener);
+    }
+
+    public void UnListenDateChanges(IDateListener listener)
+    {
+        ScheduleUtils.UnsubscribeFromDateChanges(Q, listener);
+    }
+
+    public GameEntity AnyChangeListener()
+    {
+        return ScreenUtils.GetMenu(Q);
+    }
+}
