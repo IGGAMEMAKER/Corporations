1e583ac9ac6cc9a2cead2a55ace10b160a0b69c0
gaga <23i03g@mail.ru>
2021-06-07 17:53:12 +0400

    ref

--- Assets/Standard Assets/SimpleUI/Editor/CustomInspectors/DisplayConnectedUrlsEditor.cs
+++ Assets/Standard Assets/SimpleUI/Editor/CustomInspectors/DisplayConnectedUrlsEditor.cs
@@ -57,9 +57,133 @@ namespace SimpleUI
 
             RenderUpperAndLowerRoutes(currentUrl);
             RenderReferencesToUrl(currentUrl);
-            RenderReferencesFromUrl(currentUrl);
+            //RenderReferencesFromUrl(currentUrl);
         }
 
+        void RenderReferencesFromUrl(string currentUrl)
+        {
+            //GUILayout.BeginArea(new Rect(Screen.width - w - off, off + h + off, w, h));
+            ////GUILayout.BeginArea(new Rect(off, off + h, w, h));
+            GUILayout.BeginArea(new Rect(off, off, w, h));
+
+
+            var assetPath = SimpleUI.GetOpenedAssetPath(); //
+            var matches = referencesFromAssets.Where(m => m.PrefabAssetPath.Equals(assetPath)).ToList();
+
+            if (matches.Any())
+                Label("References FROM THIS url...");
+
+            scrollPosition3 = GUILayout.BeginScrollView(scrollPosition3);
+
+            var referenceSelected = GUILayout.SelectionGrid(-1, matches.Select(m => GetPrettyNameForUrl(m.URL_ID, currentUrl)).ToArray(), 1);
+            //var referenceSelected = GUILayout.SelectionGrid(-1, matches.Select(m => GetPrettyNameForUrl(m.URL, currentUrl)).ToArray(), 1);
+
+            GUILayout.EndScrollView();
+            GUILayout.EndArea();
+
+            if (referenceSelected != -1)
+            {
+                SimpleUI.OpenPrefabByGuid(matches[referenceSelected].URL_ID);
+                //SimpleUI.OpenPrefabByUrl(matches[referenceSelected].URL);
+            }
+        }
+
+        void RenderReferencesToUrl(string currentUrl)
+        {
+            var matches = SimpleUI.WhatUsesComponent(currentUrl, referencesFromAssets);
+
+            GUILayout.BeginArea(new Rect(Screen.width - w - off, off + h + off, w, h));
+
+
+            if (matches.Any() || referencesFromCode.Any())
+                Label("References TO THIS url");
+
+            scrollPosition2 = GUILayout.BeginScrollView(scrollPosition2);
+
+            // references from prefabs & scenes
+            var names = matches.Select(m => $"<b>{SimpleUI.GetPrettyAssetType(m.PrefabAssetPath)}</b> " + SimpleUI.GetTrimmedPath(m.PrefabAssetPath)).ToList();
+            var routes = matches.Select(m => m.PrefabAssetPath).ToList();
+
+            // references from code
+            foreach (var occurence in referencesFromCode)
+            {
+                names.Add($"<b>Code</b> {SimpleUI.GetTrimmedPath(occurence.ScriptName)}");
+                routes.Add(occurence.ScriptName);
+            }
+
+            // navigate to one of these assets
+            var referenceSelected = GUILayout.SelectionGrid(-1, names.ToArray(), 1);
+
+            GUILayout.EndScrollView();
+            GUILayout.EndArea();
+
+            if (referenceSelected != -1)
+            {
+                SimpleUI.OpenPrefabByAssetPath(routes[referenceSelected]);
+            }
+        }
+
+        void RenderUpperAndLowerRoutes(string currentUrl)
+        {
+            GUILayout.BeginArea(new Rect(Screen.width - w - off, off, w, h));
+
+            Label("<b>Current url</b> and it's subroutes");
+            Label(currentUrl);
+
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+            GUILayout.BeginVertical();
+
+            //
+            var routes = new List<string>();
+            var names = new List<string>();
+
+            RenderRootLink(currentUrl, ref routes, ref names);
+            RenderSubRoutes(currentUrl, ref routes, ref names);
+
+
+            var routeSelected = GUILayout.SelectionGrid(-1, names.ToArray(), 1);
+
+            GUILayout.EndVertical();
+            GUILayout.EndScrollView();
+            GUILayout.EndArea();
+
+            if (routeSelected != -1)
+            {
+                SimpleUI.OpenPrefabByUrl(routes[routeSelected]);
+            }
+        }
+
+        //
+        void RenderSubRoutes(string currentUrl, ref List<string> routes, ref List<string> names)
+        {
+            var subRoutes = SimpleUI.GetSubUrls(currentUrl, false).OrderByDescending(p => p.Usages).ToList();
+
+            for (var i = 0; i < subRoutes.Count(); i++)
+            {
+                var pref = subRoutes[i];
+                var name = $"\u2198 {pref.Name}";
+
+                routes.Add(pref.Url);
+                names.Add(name);
+            }
+        }
+
+        void RenderRootLink(string currentUrl, ref List<string> routes, ref List<string> names)
+        {
+            var root = SimpleUI.GetUpperUrl(currentUrl);
+
+            bool hasRoot = !root.Equals(currentUrl);
+            if (hasRoot)
+            {
+                var name = $"\u2B06 ({root})";
+
+                routes.Add(root);
+                names.Add(name);
+            }
+        }
+
+        // 
         string GetPrettyNameForAsset(CountableAsset m)
         {
             string assetPath = m.AssetPath;
@@ -124,127 +248,5 @@ namespace SimpleUI
 
             return prefix + prefab.Name;
         }
-
-
-        void RenderReferencesFromUrl(string currentUrl)
-        {
-            //return;
-            GUILayout.BeginArea(new Rect(Screen.width - w - off, off + h + off, w, h));
-            //GUILayout.BeginArea(new Rect(off, off + h, w, h));
-
-            var assetPath = SimpleUI.GetOpenedAssetPath(); //
-            var matches = referencesFromAssets.Where(m => m.PrefabAssetPath.Equals(assetPath)).ToList();
-
-            if (matches.Any())
-                Label("References FROM THIS url...");
-
-            scrollPosition3 = GUILayout.BeginScrollView(scrollPosition3);
-
-            var referenceSelected = GUILayout.SelectionGrid(-1, matches.Select(m => GetPrettyNameForUrl(m.URL_ID, currentUrl)).ToArray(), 1);
-            //var referenceSelected = GUILayout.SelectionGrid(-1, matches.Select(m => GetPrettyNameForUrl(m.URL, currentUrl)).ToArray(), 1);
-
-            GUILayout.EndScrollView();
-            GUILayout.EndArea();
-
-            if (referenceSelected != -1)
-            {
-                SimpleUI.OpenPrefabByGuid(matches[referenceSelected].URL_ID);
-                //SimpleUI.OpenPrefabByUrl(matches[referenceSelected].URL);
-            }
-        }
-
-        void RenderReferencesToUrl(string currentUrl)
-        {
-            var matches = SimpleUI.WhatUsesComponent(currentUrl, referencesFromAssets);
-
-            GUILayout.BeginArea(new Rect(off, off, w, h));
-
-            if (matches.Any() || referencesFromCode.Any())
-                Label("References TO THIS url");
-
-            scrollPosition2 = GUILayout.BeginScrollView(scrollPosition2);
-
-            // references from prefabs & scenes
-            var names = matches.Select(m => $"<b>{SimpleUI.GetPrettyAssetType(m.PrefabAssetPath)}</b> " + SimpleUI.GetTrimmedPath(m.PrefabAssetPath)).ToList();
-            var routes = matches.Select(m => m.PrefabAssetPath).ToList();
-
-            // references from code
-            foreach (var occurence in referencesFromCode)
-            {
-                names.Add($"<b>Code</b> {SimpleUI.GetTrimmedPath(occurence.ScriptName)}");
-                routes.Add(occurence.ScriptName);
-            }
-
-            // navigate to one of these assets
-            var referenceSelected = GUILayout.SelectionGrid(-1, names.ToArray(), 1);
-
-            GUILayout.EndScrollView();
-            GUILayout.EndArea();
-
-            if (referenceSelected != -1)
-            {
-                SimpleUI.OpenPrefabByAssetPath(routes[referenceSelected]);
-            }
-        }
-
-        void RenderUpperAndLowerRoutes(string currentUrl)
-        {
-            GUILayout.BeginArea(new Rect(Screen.width - w - off, off, w, h));
-
-            Label("<b>Current url</b> and it's subroutes");
-            Label(currentUrl);
-
-            scrollPosition = GUILayout.BeginScrollView(scrollPosition);
-
-            GUILayout.BeginVertical();
-
-            //
-            var routes = new List<string>();
-            var names = new List<string>();
-
-            RenderRootLink(currentUrl, ref routes, ref names);
-            RenderSubRoutes(currentUrl, ref routes, ref names);
-
-
-            var routeSelected = GUILayout.SelectionGrid(-1, names.ToArray(), 1);
-
-            GUILayout.EndVertical();
-            GUILayout.EndScrollView();
-            GUILayout.EndArea();
-
-            if (routeSelected != -1)
-            {
-                SimpleUI.OpenPrefabByUrl(routes[routeSelected]);
-            }
-        }
-
-        void RenderSubRoutes(string currentUrl, ref List<string> routes, ref List<string> names)
-        {
-            var subRoutes = SimpleUI.GetSubUrls(currentUrl, false).OrderByDescending(p => p.Usages).ToList();
-
-            for (var i = 0; i < subRoutes.Count(); i++)
-            {
-                var pref = subRoutes[i];
-                var name = $"\u2198 {pref.Name}";
-
-                routes.Add(pref.Url);
-                names.Add(name);
-            }
-        }
-
-        void RenderRootLink(string currentUrl, ref List<string> routes, ref List<string> names)
-        {
-            var root = SimpleUI.GetUpperUrl(currentUrl);
-
-            bool hasRoot = !root.Equals(currentUrl);
-            if (hasRoot)
-            {
-                var name = $"\u2B06 ({root})";
-
-                routes.Add(root);
-                names.Add(name);
-            }
-        }
     }
-
 }
