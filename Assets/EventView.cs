23d5457d8c16d8f016ba4cdcdb72730b541a3459
gaga <23i03g@mail.ru>
2021-07-15 21:54:01 +0400

    Interrupts to main screen

--- Assets/EventView.cs
+++ Assets/EventView.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using Assets.Core;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+public enum EventStatusType
+{
+    Possibility,
+    Warning,
+    Danger,
+
+    Message
+}
+
 
 public class EventView : View
 {
@@ -11,10 +22,32 @@ public class EventView : View
 
     public string title;
     public int counter;
-    
+
+    public Image EventStatus;
+    public EventStatusType StatusType;
+
     private void OnValidate()
     {
         Title.text = title;
         Counter.text = $"({counter})";
+
+        if (StatusType == EventStatusType.Danger)
+        {
+            EventStatus.color = Visuals.Negative();
+        }
+        else if (StatusType == EventStatusType.Possibility)
+        {
+            EventStatus.color = Visuals.Positive();
+        }
+        else if (StatusType == EventStatusType.Warning)
+        {
+            EventStatus.color = Color.magenta;
+        }
+        else
+        {
+            EventStatus.color = Visuals.Neutral();
+        }
+
+        Draw(EventStatus, StatusType != EventStatusType.Message);
     }
 }
